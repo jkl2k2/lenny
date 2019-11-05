c4442d8948c8fbcff0cbe3c8c845860fa80db7ef
Joshua <joshualomelin@gmail.com>
2019-11-04 21:35:11 -0800

    Added command handler to index.js
    Added commands: ping, help
    
    Installed discord.js, node-opus, ffmpeg

---
+++ index.js
@@ -0,0 +1,93 @@
+// Load discord.js library and load config
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token, api, ownerID, jahyID } = require('./config.json');
+
+// Initialize client
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const cooldowns = new Discord.Collection();
+
+// Load all command files
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+// On ready
+client.once('ready', () => {
+    console.log("// Bot initialized //");
+});
+
+
+// On message
+client.on('message', message => {
+    // Return if no prefix or said by bot
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    // Put args into array
+    const args = message.content.slice(prefix.length).split(/ +/);
+    
+    // Extract command name
+    const commandName = args.shift().toLowerCase();
+    
+    // Check if valid command or alias
+    const command = client.commands.get(commandName)
+        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    // Return if not valid command
+    if (!command) return;
+
+    // If guild-only, no DMs allowed
+    if (command.guildOnly && message.channel.type !== 'text') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }    
+
+    // If command needs arguments
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+
+        if (command.usage) {
+        reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+	return message.channel.send(reply);
+    }
+
+    // If command has cooldowns
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+    
+    // Set times for use with cooldown
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    // Act on cooldown
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+    
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    }    
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+});
+
+client.login(token);

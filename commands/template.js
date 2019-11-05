cad72e2007139d3ed6145e41e2540341075de371
Joshua <joshualomelin@gmail.com>
2019-11-04 22:13:53 -0800

    Filled out command template

---
+++ commands/template.js
@@ -0,0 +1,12 @@
+// Any 'require'
+
+module.exports = {
+	name: 'name',
+	description: 'Description',
+	// aliases: ['aliases'],
+	usage: '[command]',
+	// cooldown: 5,
+	execute(message, args) {
+        // Start here
+    }
+}

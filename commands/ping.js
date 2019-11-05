2085df7d3cfc63d5221883e676a2b320bb4e836f
Joshua <joshualomelin@gmail.com>
2019-11-04 22:06:17 -0800

    Adapted ping code from old bot

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,21 @@
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
-	execute(message, args) {
-		message.channel.send('Pong.');
+	async execute(message, args) {
+		let pingEmbed = new Discord.RichEmbed()
+			.setTitle(`:arrows_counterclockwise: Testing latency...`)
+			.setColor(`#0083FF`)
+
+		const m = await message.channel.send(pingEmbed);
+
+		let newPingEmbed = new Discord.RichEmbed()
+			.setTitle(`:white_check_mark: Latency Test Completed`)
+			.setColor(`#44C408`)
+			.addField(`Frontend Latency`, `${m.createdTimestamp - message.createdTimestamp}ms`)
+			.addField(`Discord API Latency`, `${Math.round(message.client.ping)}ms`)
+
+		m.edit(newPingEmbed);
 	},
 };

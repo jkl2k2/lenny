10ce4094630722ca00ff3f8174f4b7da8aeaff1d
Joshua <joshualomelin@gmail.com>
2019-11-05 16:53:46 -0800

    Added leave command

---
+++ commands/leave.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'leave',
+	description: `Leaves the user's voice channel`,
+	// aliases: ['aliases'],
+	usage: '[no arguments]',
+	// cooldown: seconds,
+	execute(message, args) {
+        /*
+        if(dispatcher != undefined && dispatcher.speaking) {
+			let leaveFailEmbed = new Discord.RichEmbed()
+			.setTitle(`:no_entry: **I cannot leave a channel while playing music, ${message.author.username}**`)
+			.setColor(`#FF0000`)
+			message.channel.send(leaveFailEmbed);
+
+			return;
+        }
+        */
+		message.member.voiceChannel.leave();
+		let leaveEmbed = new Discord.RichEmbed()
+			.setTitle(`:white_check_mark: **I left your channel, ${message.author.username}**`)
+			.setColor(`#44C408`)
+		message.channel.send(leaveEmbed);
+    }
+}

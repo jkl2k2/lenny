472f8d61b0bf7678d8338e4994ca968c746f29df
Joshua <joshualomelin@gmail.com>
2019-11-07 22:42:57 -0800

    Added queue command

---
+++ commands/queue.js
@@ -0,0 +1,12 @@
+const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
+
+module.exports = {
+	name: 'queue',
+	description: 'Returns the current music queue (up to 5 songs)',
+	// aliases: ['aliases'],
+	// usage: '[command]',
+	// cooldown: 5,
+	execute(message, args) {
+        index.viewQueue();
+    }
+}

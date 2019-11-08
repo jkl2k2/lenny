d03aa46a6b9541e63cfe00e557733524fb5b4410
Joshua <joshualomelin@gmail.com>
2019-11-07 23:37:21 -0800

    Added shuffle command

---
+++ commands/shuffle.js
@@ -0,0 +1,13 @@
+const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
+// Any 'require'
+
+module.exports = {
+	name: 'shuffle',
+	description: 'Shuffles the music queue',
+	// aliases: ['aliases'],
+	// usage: '[command]',
+	// cooldown: 5,
+	execute(message, args) {
+        index.shuffleQueue(message);
+    }
+}

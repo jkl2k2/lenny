cb409f02507bcda948c80f28b56e6cc646caa453
Joshua <joshualomelin@gmail.com>
2019-11-07 22:43:02 -0800

    Added remove command

---
+++ commands/remove.js
@@ -0,0 +1,14 @@
+const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
+// Any 'require'
+
+module.exports = {
+	name: 'remove',
+	description: 'Removes a video from the queue',
+	aliases: ['queueremove'],
+	usage: '[video number in queue]',
+	// cooldown: 5,
+	execute(message, args) {
+        args.unshift();
+        index.removeFromQueue(args[0]);
+    }
+}

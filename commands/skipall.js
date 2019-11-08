fac705e1bf4eef33e5548602511f5e3f9d512d74
Joshua <joshualomelin@gmail.com>
2019-11-07 23:38:04 -0800

    Just like skip.js, endDispatcher() is now callEndDispatcher()

---
+++ commands/skipall.js
@@ -0,0 +1,12 @@
+const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
+
+module.exports = {
+	name: 'skipall',
+	description: 'Empties the queue and skips the current song',
+	// aliases: ['aliases'],
+	// usage: '[command]',
+	// cooldown: 5,
+	execute(message, args) {
+        index.callEndDispatcher(message.channel, message.author.username, "skipall");
+    }
+}

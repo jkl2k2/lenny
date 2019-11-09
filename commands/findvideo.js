d7d79fff7162e281fa21b302555d9ee66edcc2c3
Joshua <joshualomelin@gmail.com>
2019-11-08 19:50:14 -0800

    Added findvideo command

---
+++ commands/findvideo.js
@@ -0,0 +1,35 @@
+const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
+const Discord = require(`discord.js`)
+const { prefix, token, api, ownerID, jahyID } = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\config.json`);
+const { YouTube } = require('better-youtube-api');
+const youtube = new YouTube(api);
+
+module.exports = {
+    name: 'findvideo',
+    description: 'Searches YouTube with the provided search terms, then returns the best match',
+    aliases: ['find'],
+    usage: '[search term(s)]',
+    // cooldown: 5,
+    guildOnly: true,
+    async execute(message, args) {
+        var manualVideoSearch = await youtube.searchVideos(args.join()).catch(function (error) {
+            console.error(`${error}\nTimestamp of error: ${timestamp('MM-DD-YYYY hh:mm:ss')}`);
+        });
+        if (manualVideoSearch[0] != undefined && args.length >= 1) {
+            message.channel.send(`**Best match:**\n${manualVideoSearch[0].url}`);
+        } else if (args.length == 0) {
+            let noArgsSearchEmbed = new Discord.RichEmbed()
+                .setTitle(`:no_entry: **${message.author.username}, you need to provide search terms**`)
+                .setColor(`#FF0000`)
+            message.channel.send(noArgsSearchEmbed);
+        } else {
+            let searchFailEmbed = new Discord.RichEmbed()
+                .setTitle(` `)
+                .addField(`:no_entry: **Sorry, ${message.author.username}, I couldn't find any videos with those search terms**`, `Please try again with different search terms`)
+                .setTimestamp()
+                .setFooter(`Requested by ${message.author.username}`)
+                .setColor(`#FF0000`)
+            message.channel.send(searchFailEmbed);
+        }
+    }
+}

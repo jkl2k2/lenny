7c59b0a537179033945255254c88db34758c8d50
Joshua <joshualomelin@gmail.com>
2019-11-09 21:53:48 -0800

    Added resume command

---
+++ commands/resume.js
@@ -0,0 +1,14 @@
+const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
+// Any 'require'
+
+module.exports = {
+	name: 'resume',
+	description: 'Resumes music playback if it was paused',
+	// aliases: ['aliases'],
+	// usage: '[command]',
+	// cooldown: 5,
+	guildOnly: true,
+	execute(message, args) {
+		index.pauseMusic(message);
+	}
+}

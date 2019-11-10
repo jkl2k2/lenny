cf8c655a14d4c47bcf4ed9e588f47703e5bd18d0
Joshua <joshualomelin@gmail.com>
2019-11-09 21:49:39 -0800

    Added pause command

---
+++ commands/pause.js
@@ -0,0 +1,14 @@
+const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
+// Any 'require'
+
+module.exports = {
+	name: 'template',
+	description: 'Template command for easier coding, does nothing',
+	// aliases: ['aliases'],
+	// usage: '[command]',
+	// cooldown: 5,
+	guildOnly: true,
+	execute(message, args) {
+		index.pauseMusic(message);
+	}
+}

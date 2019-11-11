224f1547097599defa5ae85dde5f658f17543270
Joshua <joshualomelin@gmail.com>
2019-11-11 00:15:10 -0800

    Pause now mostly occurs in command file

--- commands/pause.js
+++ commands/pause.js
@@ -1,5 +1,5 @@
 const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
-// Any 'require'
+const Discord = require(`discord.js`);
 
 module.exports = {
 	name: 'pause',
@@ -9,6 +9,21 @@ module.exports = {
 	// cooldown: 5,
 	guildOnly: true,
 	execute(message, args) {
-		index.pauseMusic(message);
+		// index.pauseMusic(message);
+
+		var dispatcher = index.getDispatcher();
+
+		if (dispatcher != undefined && dispatcher.paused == false) {
+			index.pauseMusic();
+			let pauseEmbed = new Discord.RichEmbed()
+				.setTitle(`:pause_button: ${message.author.username} paused playback`)
+				.setColor(`#44C408`)
+			message.channel.send(pauseEmbed);
+		} else {
+			let pauseFailEmbed = new Discord.RichEmbed()
+				.setTitle(`<:error:643341473772863508> ${message.author.username}, the music is already paused`)
+				.setColor(`#FF0000`)
+			message.channel.send(pauseFailEmbed);
+		}
 	}
 }

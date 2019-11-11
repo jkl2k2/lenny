2fe7023f6e323b0d94df309cf52fadbba59ca025
Joshua <joshualomelin@gmail.com>
2019-11-11 00:15:45 -0800

    Queue mostly happens inside of command file

--- commands/queue.js
+++ commands/queue.js
@@ -1,4 +1,21 @@
 const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
+const Discord = require(`discord.js`);
+
+function queueResolver(arr, index) {
+    if (arr[index]) {
+        return `${index + 1}. [${arr[index].getTitle()}](${arr[index].getURL()})`;
+    } else {
+        return " ";
+    }
+}
+
+function queueOverflowResolver(arr) {
+    if (arr.length <= 5) {
+        return " ";
+    } else if (arr.length > 5) {
+        return `**Plus ${arr.length - 5} more songs**`;
+    }
+}
 
 module.exports = {
 	name: 'queue',
@@ -8,6 +25,23 @@ module.exports = {
 	// cooldown: 5,
 	guildOnly: true,
 	execute(message, args) {
-		index.viewQueue();
+
+		var queue = index.getQueue();
+
+		if (queue.length == 0) {
+			let emptyQueueEmbed = new Discord.RichEmbed()
+				.setTitle(` `)
+				.addField(`:information_source: **Current queue**`, `The queue is currently empty`)
+				.setColor(`#0083FF`)
+			message.channel.send(emptyQueueEmbed);
+		} else {
+			let queueEmbed = new Discord.RichEmbed()
+				.setTitle(` `)
+				// .setDescription(`${queueResolver(parsedQueue, 0)}\n\n${queueResolver(parsedQueue, 1)}\n\n${queueResolver(parsedQueue, 2)}\n\n${queueResolver(parsedQueue, 3)}\n\n${queueResolver(parsedQueue, 4)}\n\n${queueOverflowResolver(parsedQueue)}`)
+				.addField(`**:information_source: Current queue**`, `${queueResolver(queue, 0)}\n\n${queueResolver(queue, 1)}\n\n${queueResolver(queue, 2)}\n\n${queueResolver(queue, 3)}\n\n${queueResolver(queue, 4)}\n\n${queueOverflowResolver(queue)}`)
+				.setColor(`#0083FF`)
+			// message.channel.send(`Current queue: ${parsedQueue[0]}\n\nComing up next: ${parsedQueue[1]}`);
+			message.channel.send(queueEmbed);
+		}
 	}
 }

f9ed058f88c217d8353f4608415836998535648e
Joshua <joshualomelin@gmail.com>
2019-11-11 00:16:08 -0800

    Shuffle mostly happens in command file

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,5 +1,22 @@
 const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
-// Any 'require'
+const Discord = require(`discord.js`);
+
+// Fisher-Yates Shuffle
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
 
 module.exports = {
 	name: 'shuffle',
@@ -9,6 +26,25 @@ module.exports = {
 	// cooldown: 5,
 	guildOnly: true,
 	execute(message, args) {
-		index.shuffleQueue(message);
+		// index.shuffleQueue(message);
+
+		var queue = index.getQueue();
+
+        if (queue.length > 0) {
+			var shuffled = shuffle(queue);
+			index.setQueue(shuffled);
+
+            let shuffleCompleteEmbed = new Discord.RichEmbed()
+				.setTitle(` `)
+				.addField(`:white_check_mark: Shuffled queue`, `Shuffled ${queue.length} songs in queue`)
+                .setColor(`#44C408`)
+            message.channel.send(shuffleCompleteEmbed);
+        } else {
+            let shuffleFailEmbed = new Discord.RichEmbed()
+				.setTitle(` `)
+				.addField(`<:error:643341473772863508> Shuffle failed`, `Cannot shuffle an empty queue`)
+                .setColor(`#FF0000`)
+            message.channel.send(shuffleFailEmbed);
+        }
 	}
 }

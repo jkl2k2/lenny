16389b1af61c1f454869bff7fdcc45b36c54c105
Joshua <joshualomelin@gmail.com>
2019-11-11 00:14:25 -0800

    Can now leave without requester being in same VC

--- commands/leave.js
+++ commands/leave.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
+var client = index.getClient();
 
 module.exports = {
 	name: 'leave',
@@ -8,20 +10,25 @@ module.exports = {
 	// cooldown: seconds,
 	guildOnly: true,
 	execute(message, args) {
-        /*
-        if(dispatcher != undefined && dispatcher.speaking) {
-			let leaveFailEmbed = new Discord.RichEmbed()
-			.setTitle(`:no_entry: **I cannot leave a channel while playing music, ${message.author.username}**`)
-			.setColor(`#FF0000`)
-			message.channel.send(leaveFailEmbed);
 
-			return;
-        }
-        */
-		message.member.voiceChannel.leave();
-		let leaveEmbed = new Discord.RichEmbed()
-			.setTitle(`:white_check_mark: **I left your channel, ${message.author.username}**`)
-			.setColor(`#44C408`)
-		message.channel.send(leaveEmbed);
+		var connectionArray = client.voiceConnections.array();
+
+		if (connectionArray[0] != undefined) {
+			connectionArray[0].disconnect();
+
+			let leaveEmbed = new Discord.RichEmbed()
+				.setTitle(` `)
+				.addField(`:white_check_mark: Left voice channel`, `**I left the voice channel**`)
+				.setColor(`#44C408`)
+
+			message.channel.send(leaveEmbed);
+		} else {
+			let leaveFailEmbed = new Discord.RichEmbed()
+				.setTitle(` `)
+				.addField(`<:error:643341473772863508> Failed to leave`, `**I'm not in a voice channel**`)
+				.setColor(`#FF0000`)
+
+			message.channel.send(leaveFailEmbed);
+		}
 	}
 }

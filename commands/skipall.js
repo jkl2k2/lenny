58836451d22ba5ea30c2b25fa185e295127e7cf7
Joshua <joshualomelin@gmail.com>
2019-11-11 01:37:21 -0800

    Almost all of skipall occurs in command file

--- commands/skipall.js
+++ commands/skipall.js
@@ -1,4 +1,5 @@
 const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
+const Discord = require(`discord.js`);
 
 module.exports = {
 	name: 'skipall',
@@ -8,6 +9,27 @@ module.exports = {
 	// cooldown: 5,
 	guildOnly: true,
 	execute(message, args) {
-		index.callEndDispatcher(message.channel, message.author.username, "skipall");
+		
+		var dispatcher = index.getDispatcher();
+
+		if (dispatcher == undefined || dispatcher.speaking == false) {
+			let skipFailEmbed = new Discord.RichEmbed()
+				.setTitle(` `)
+				.addField(`<:error:643341473772863508> Skipall failed`, `There is nothing to skip`)
+				.setColor(`#FF0000`)
+			message.channel.send(skipFailEmbed);
+
+			return;
+		}
+
+		index.setQueue([]);
+		index.endDispatcher();
+
+		let endDispatcherEmbed = new Discord.RichEmbed()
+			.setTitle(` `)
+			.addField(`:fast_forward: Skipped all songs`, `${message.author.username} skipped all songs in queue`)
+			.setColor(`#44C408`)
+		message.channel.send(endDispatcherEmbed);
+
 	}
 }

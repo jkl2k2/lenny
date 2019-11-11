46ae09d822ccc9408f6faf2cb299cae9f9cd2d88
Joshua <joshualomelin@gmail.com>
2019-11-11 01:37:11 -0800

    Almost all of skip occurs in command file

--- commands/skip.js
+++ commands/skip.js
@@ -1,4 +1,5 @@
 const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
+const Discord = require(`discord.js`);
 
 module.exports = {
 	name: 'skip',
@@ -8,6 +9,27 @@ module.exports = {
 	// cooldown: 5,
 	guildOnly: true,
 	execute(message, args) {
-		index.callEndDispatcher(message.channel, message.author.username, "skip");
+		// index.callEndDispatcher(message.channel, message.author.username, "skip");
+
+		var dispatcher = index.getDispatcher();
+
+		if (dispatcher == undefined || dispatcher.speaking == false) {
+			let skipFailEmbed = new Discord.RichEmbed()
+				.setTitle(` `)
+				.addField(`<:error:643341473772863508> Skip failed`, `There is nothing to skip`)
+				.setColor(`#FF0000`)
+			message.channel.send(skipFailEmbed);
+
+			return;
+		}
+
+		index.endDispatcher();
+
+		let endDispatcherEmbed = new Discord.RichEmbed()
+			.setTitle(` `)
+			.addField(`:fast_forward: Skipped song`, `${message.author.username} skipped the current song`)
+			.setColor(`#44C408`)
+		message.channel.send(endDispatcherEmbed);
+
 	}
 }

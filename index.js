8db292d8d5955ae9347357d8a29960446e9af262
Joshua <joshualomelin@gmail.com>
2019-11-11 01:34:46 -0800

    Almost all music stuff moved to command files
    Removed unnecessary requires
    
    Removed unnecessary commented code
    
    Removed callEndDispatcher and callQueueRead from exports
    
    Removed serverMessage, no longer needed
    
    playMusic() now takes a message and keeps track of last music message

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token, api, ownerID, jahyID } = require('./config.json');
 const ytdl = require("ytdl-core-discord");
-const { YouTube } = require('better-youtube-api');
-const youtube = new YouTube(api);
-var ffmpeg = require('ffmpeg-static');
 const prism = require('prism-media');
 
 var queue = [];
@@ -38,73 +35,7 @@ const activities_list_types = [
 
 var dispatcher;
 
-var serverMessage;
-
-class YTVideo {
-    constructor(video, requester) {
-        this.video = video;
-        this.requester = requester;
-    }
-    getTitle() {
-        return this.video.title;
-    }
-    getURL() {
-        return this.video.url;
-    }
-    getRequester() {
-        return this.requester;
-    }
-    getRequesterName() {
-        return this.requester.username;
-    }
-    getType() {
-        return this.video.liveStatus;
-    }
-    getThumbnail() {
-        return this.video.thumbnails.default.url;
-    }
-    async getChannelName() {
-        var id = `${this.video.channelId}`;
-        var resolved = await youtube.getChannel(id);
-        return resolved.name;
-    }
-    getChannelURL() {
-        return `https://www.youtube.com/channel/${this.video.channelId}`;
-    }
-    getVideo() {
-        return this.video;
-    }
-}
-
-function endDispatcher(c, author, method) {
-    if (dispatcher != undefined && dispatcher.speaking == true) {
-        if (method == "skip") {
-            let endDispatcherEmbed = new Discord.RichEmbed()
-                .setTitle(`:fast_forward: **${author} skipped the current song**`)
-                .setColor(`#44C408`)
-            c.send(endDispatcherEmbed);
-        } else if (method == "skipall") {
-            queue = [];
-            let endDispatcherEmbed = new Discord.RichEmbed()
-                .setTitle(`:fast_forward: **${author} emptied the queue and skipped everything**`)
-                .setColor(`#44C408`)
-            c.send(endDispatcherEmbed);
-        } else if (method == "playnow") {
-            let endDispatcherEmbed = new Discord.RichEmbed()
-                .setTitle(`:fast_forward: **${author} force played a song**`)
-                .setColor(`#44C408`)
-            c.send(endDispatcherEmbed);
-        }
-
-        dispatcher.end();
-
-    } else {
-        let endDispatcherEmbed = new Discord.RichEmbed()
-            .setTitle(`:eyes: ${author}, there's nothing to skip, dumbo`)
-            .setColor(`#FF0000`)
-        c.send(endDispatcherEmbed);
-    }
-}
+var lastMusicMessage;
 
 async function sendDetails(input, c) {
     var musicEmbed = new Discord.RichEmbed()
@@ -118,9 +49,10 @@ async function sendDetails(input, c) {
     c.send(musicEmbed);
 }
 
-async function playMusic(disabled) {
+async function playMusic(message) {
     // console.log(message);
-    // console.log(serverMessage);
+    // console.log(lastMusicMessage);
+
     if (queue == undefined) {
         console.log("playMusic() called, but queue undefined");
         return;
@@ -130,157 +62,30 @@ async function playMusic(disabled) {
         return;
     }
     if (queue[0].getType() == undefined || queue[0].getType() == false) {
-        // dispatcher = serverMessage.member.voiceChannel.connection.playOpusStream(await ytdl(queue[0].videoUrl));
         let input = await ytdl(queue[0].getURL());
         const pcm = input.pipe(new prism.opus.Decoder({ rate: 48000, channels: 2, frameSize: 960 }));
-        // dispatcher = serverMessage.member.voiceChannel.connection.playConvertedStream(pcm);
         var connectionArray = client.voiceConnections.array();
         dispatcher = connectionArray[0].playConvertedStream(pcm);
 
     } else if (queue[0].getType() == "live") {
-        // dispatcher = serverMessage.member.voiceChannel.connection.playOpusStream(await ytdl(queue[0].videoUrl, {quality: 95}));
         let input = await ytdl(queue[0].getURL(), { quality: 93 });
         const pcm = input.pipe(new prism.opus.Decoder({ rate: 48000, channels: 2, frameSize: 960 }));
-        // dispatcher = serverMessage.member.voiceChannel.connection.playConvertedStream(pcm);
         var connectionArray = client.voiceConnections.array();
         dispatcher = connectionArray[0].playConvertedStream(pcm);
     } else {
-        serverMessage.channel.send("Error assigning dispatcher");
+        message.channel.send("Error assigning dispatcher");
     }
 
-    sendDetails(queue[0], serverMessage.channel);
+    sendDetails(queue[0], message.channel);
     queue.shift();
 
-    serverMessage.member.voiceChannel.connection.player.streamingData.pausedTime = 0;
+    message.member.voiceChannel.connection.player.streamingData.pausedTime = 0;
 
     dispatcher.on("end", function () {
         if (queue[0]) {
-            playMusic();
+            playMusic(message);
         }
     });
-}
-
-function handleVC(discordMessage) {
-    if (discordMessage.member.voiceChannel) {
-        discordMessage.member.voiceChannel.join()
-            .then(connection => {
-                if (!connection.speaking) {
-                    setTimeout(function () {
-                        playMusic(discordMessage);
-                    }, 250);
-
-                }
-            })
-            .catch(`${console.log} Timestamp: timestamp`);
-    } else {
-        let vcFailEmbed = new Discord.RichEmbed()
-            .setTitle(`:warning: ${discordMessage.author.username}, you are not in a voice channel. Your video has been queued, but I am unable to join you.`)
-            .setColor(`#FF0000`)
-        discordMessage.channel.send(vcFailEmbed);
-    }
-}
-
-async function handlePlaylist(message, args) {
-    var playlistInfo = await youtube.getPlaylist(args[0]);
-    var playlistArray = await youtube.getPlaylistItems(args[0]).catch(function (error) {
-        console.error(`${error}`);
-    });
-
-    var listEmbed = new Discord.RichEmbed()
-        .setColor(`#00c292`)
-        .setTitle(` `)
-        .addField(`:arrow_up_small: **Playlist added to queue (${playlistInfo.length} songs)**`, `[${playlistInfo.title}](${args[0]})`)
-        .setThumbnail(playlistInfo.thumbnails.default.url)
-        .setTimestamp()
-        .setFooter(`Requested by ${message.author.username}`)
-    message.channel.send(listEmbed);
-
-    var listProcessingEmbed = new Discord.RichEmbed()
-        .setTitle(`:arrows_counterclockwise: Please wait while your playlist is being processed`)
-        .setColor(`#FF0000`)
-
-    var listProcessingMessage = await message.channel.send(listProcessingEmbed);
-
-    for (var i = 0; i < playlistArray.length; i++) {
-        // let playlistVideo = new YTVideo(await playlistArray[i].title, await playlistArray[i].url, playlistArray[i].liveStatus, message.author);
-        let playlistVideo = new YTVideo(playlistArray[i], message.author);
-        queue.push(playlistVideo);
-
-        // DEBUG - CAUSES SPAM
-        // message.channel.send(`EXPECTED OUTCOME:\n\nQueued video with title ${await playlistArray.videos[i].title}\nURL of queued video is: ${await playlistArray.videos[i].url}\n\nRESULT:\n\nQueued video with title ${await videoRequestObject.videoTitle}\nURL of queued video is: ${await videoRequestObject.videoUrl}`);
-
-    }
-    let newProcessingEmbed = new Discord.RichEmbed()
-        .setTitle(`:white_check_mark: The playlist has finished processing!`)
-        .setColor(`#44C408`)
-    listProcessingMessage.edit(newProcessingEmbed);
-    // message.channel.send(newProcessingEmbed);
-}
-
-async function handleVideoNoPlaylist(method, message, args) {
-    var videoResult = await youtube.getVideo(args.join(" "));
-
-    // let newVideo = new YTVideo(videoResult.title, videoResult.url, videoResult.liveStatus, message.author);
-    let newVideo = new YTVideo(videoResult, message.author);
-    if (method === "playnow") {
-        queue.unshift(newVideo);
-        endDispatcher(message.channel, message.author.username, "playnow");
-    } else if (method === "playnext") {
-        queue.unshift(newVideo);
-    } else {
-        queue.push(newVideo);
-    }
-
-    var playEmbed = new Discord.RichEmbed()
-        .setColor(`#00c292`)
-        .setTitle(` `)
-        .addField(`**:arrow_up_small: Queued**`, `[${newVideo.getTitle()}](${newVideo.getURL()})`)
-        .addField(`Uploader`, `[${await newVideo.getChannelName()}](${newVideo.getChannelURL()})`)
-        .setThumbnail(newVideo.getThumbnail())
-        .setTimestamp()
-        .setFooter(`Requested by ${newVideo.getRequesterName()}`)
-    message.channel.send(playEmbed);
-
-}
-
-function readIndexQueue(thingToPush) {
-    queue.push(thingToPush);
-    console.log(queue);
-}
-
-function onlyJoinVC(message) {
-    if (message.member.voiceChannel) {
-        message.member.voiceChannel.join();
-        let joinEmbed = new Discord.RichEmbed()
-            .setTitle(`:white_check_mark: **I joined your channel, ${message.author.username}**`)
-            .setColor(`#44C408`)
-        message.channel.send(joinEmbed);
-    } else {
-        message.channel.send(`:eyes: ${message.author}, I can't join your VC if you're not in a VC, ya doofus`);
-    }
-}
-
-async function handlePlayCommand(method, message, args) {
-    if (args[0] == undefined) {
-        let undefArgsEmbed = new Discord.RichEmbed()
-            .setTitle(`:eyes: ${message.author.username}, please include at least one search term or URL`)
-            .setColor(`#FF0000`)
-        message.channel.send(undefArgsEmbed);
-
-        return;
-    }
-
-    if (args[0].includes("playlist?list=")) {
-        handlePlaylist(message, args);
-        setTimeout(function () {
-            handleVC(message);
-        }, 1000)
-    } else {
-        handleVideoNoPlaylist(method, message, args);
-        setTimeout(function () {
-            handleVC(message);
-        }, 500)
-    }
 }
 
 // Functions
@@ -312,14 +117,14 @@ module.exports = {
     resumeMusic: function () {
         dispatcher.resume();
     },
-    callEndDispatcher: function (c, author, method) {
-        endDispatcher(c, author, method);
-    },
-    callQueueRead: function (toPush) {
-        readIndexQueue(toPush);
+    endDispatcher: function () {
+        dispatcher.end();
     },
     playCommand: function (method, message, args) {
         handlePlayCommand(method, message, args);
+    },
+    callPlayMusic: function (message) {
+        playMusic(message);
     }
 };
 
@@ -346,8 +151,6 @@ client.once('ready', () => {
 client.on('message', message => {
     // Return if no prefix or said by bot
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    serverMessage = message;
 
     // Put args into array
     const args = message.content.slice(prefix.length).split(/ +/);

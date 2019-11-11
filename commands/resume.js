cb1f4e4487d69e33a19eae4e18c831471484e096
Joshua <joshualomelin@gmail.com>
2019-11-11 00:16:01 -0800

    Resume mostly happens in command file

--- commands/resume.js
+++ commands/resume.js
@@ -1,5 +1,5 @@
 const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
-// Any 'require'
+const Discord = require(`discord.js`);
 
 module.exports = {
 	name: 'resume',
@@ -9,6 +9,21 @@ module.exports = {
 	// cooldown: 5,
 	guildOnly: true,
 	execute(message, args) {
-		index.resumeMusic(message);
+		// index.resumeMusic(message);
+
+		var dispatcher = index.getDispatcher();
+
+		if (dispatcher != undefined && dispatcher.paused == true) {
+			index.resumeMusic();
+			let resumeEmbed = new Discord.RichEmbed()
+				.setTitle(`:arrow_forward: ${message.author.username} resumed playback`)
+				.setColor(`#44C408`)
+			message.channel.send(resumeEmbed);
+		} else {
+			let resumeFailEmbed = new Discord.RichEmbed()
+				.setTitle(`<:error:643341473772863508> ${message.author.username}, the music is already playing`)
+				.setColor(`#FF0000`)
+			message.channel.send(resumeFailEmbed);
+		}
 	}
 }

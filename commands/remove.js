81d6ecdfc7557d5003ff2c73fc74fa92fd9c988b
Joshua <joshualomelin@gmail.com>
2019-11-11 00:15:54 -0800

    Remove mostly happens inside command file

--- commands/remove.js
+++ commands/remove.js
@@ -1,5 +1,5 @@
 const index = require(`C:\\Users\\Joshua\\Desktop\\Programming Stuff\\L-Bot Files\\L-Bot-Rewrite\\index.js`);
-// Any 'require'
+const Discord = require(`discord.js`);
 
 module.exports = {
 	name: 'remove',
@@ -10,6 +10,42 @@ module.exports = {
 	guildOnly: true,
 	execute(message, args) {
 		args.unshift();
-		index.removeFromQueue(args[0]);
+		// index.removeFromQueue(args[0]);
+
+		var queue = index.getQueue();
+
+		var target = args[0];
+
+        if (queue[target - 1] == undefined) {
+            let indexDNEEmbed = new Discord.RichEmbed()
+                .setTitle(`<:error:643341473772863508> Index ${target - 1} of array with given input ${target} does not exist`)
+                .setTimestamp()
+                .setFooter(`Requested by ${message.author.username}`)
+                .setColor(`#FF0000`);
+            message.channel.send(indexDNEEmbed);
+            return;
+        }
+        var elementToRemove = queue[target - 1];
+        queue.splice(target - 1, 1);
+        if (elementToRemove != queue[target]) {
+            let queueRemoveEmbed = new Discord.RichEmbed()
+                .setTitle(` `)
+                .addField(`:white_check_mark: **Successfully removed from queue**`, `[${elementToRemove.getTitle()}](${elementToRemove.getURL()})`)
+                .setTimestamp()
+                .setFooter(`Requested by ${message.author.username}`)
+                .setColor(`#44C408`)
+            message.channel.send(queueRemoveEmbed);
+            // message.reply(`successfully removed "${elementToRemove.videoTitle}" from queue!`);
+        } else {
+            let queueRemoveEmbed = new Discord.RichEmbed()
+                .setTitle(`Somehow, I failed to remove "[${elementToRemove.getTitle()}](${elementToRemove.getURL()})" from queue. This should never happen.`)
+                .setTimestamp()
+                .setFooter(`Requested by ${message.author.username}`)
+                .setColor(`#FF0000`);
+            message.channel.send(queueRemoveEmbed);
+            // message.reply(`:thinking: I don't understand.`);
+        }
+
+		index.setQueue(queue);
 	}
 }

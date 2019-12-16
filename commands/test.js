e2e69b02379b473403ff0f37da419b63636943ac
Joshua <joshualomelin@gmail.com>
2019-12-15 19:14:17 -0800

    Used !test to view SC track info, but commented out

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,7 @@
 const index = require(`../index.js`);
 const fs = require('fs');
+const youtubedl = require('youtube-dl');
+const Discord = require('discord.js');
 
 module.exports = {
     name: 'test',
@@ -9,5 +11,24 @@ module.exports = {
     // cooldown: 5,
     async execute(message, args) {
         message.channel.send("Test message!");
+
+        /*
+        const video = youtubedl(args[0]);
+
+        video.on('info', async function (info) {
+            let scDownload = new Discord.RichEmbed()
+                .setTitle(` `)
+                .addField(`:arrows_counterclockwise: Downloading SoundCloud song`, `[${info._filename.substring(0, (info._filename.length) - 14)}](${info.webpage_url})`)
+                .addField(`Uploader`, `[${info.uploader}](${info.uploader_url})`, true)
+                .addField(`Length`, info._duration_hms.substring(3, 8), true)
+                .setColor(`#0083FF`)
+            message.channel.send(scDownload);
+            console.log('Download started');
+            console.log('filename: ' + info._filename);
+            console.log('size: ' + info.size);
+            
+            console.log(info);
+        });
+        */
     }
 }

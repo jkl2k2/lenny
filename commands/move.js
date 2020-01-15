d4c3b5f542318852b1b56d5521e3ac26a5e52a3b
Joshua <joshualomelin@gmail.com>
2020-01-14 19:32:55 -0800

    Check if !move args are valid or exist All messages are now embeds

--- commands/move.js
+++ commands/move.js
@@ -13,6 +13,34 @@ module.exports = {
         var startPos = args[0] - 1;
         var targetPos = args[1] - 1;
 
+        if (!args[0]) {
+            let noStart = new Discord.RichEmbed()
+                .setDescription(`<:error:643341473772863508> Please specify a start position`)
+                .setColor(`#FF0000`)
+            message.channel.send(noStart);
+            return;
+        } else if (!args[1]) {
+            let noTarget = new Discord.RichEmbed()
+                .setDescription(`<:error:643341473772863508> Please specify a target position`)
+                .setColor(`#FF0000`)
+            message.channel.send(noTarget);
+            return;
+        }
+
+        if (isNaN(args[0])) {
+            let startNaN = new Discord.RichEmbed()
+                .setDescription(`<:error:643341473772863508> Start position must be a number`)
+                .setColor(`#FF0000`)
+            message.channel.send(startNaN);
+            return;
+        } else if (isNaN(args[1])) {
+            let targetNaN = new Discord.RichEmbed()
+                .setDescription(`<:error:643341473772863508> Target position must be a number`)
+                .setColor(`#FF0000`)
+            message.channel.send(targetNaN);
+            return;
+        }
+
         // If target video exists at that position
         if (queue[startPos]) {
             // If target position is valid
@@ -31,10 +59,16 @@ module.exports = {
                     message.channel.send(moveSuccess);
                 }
             } else {
-                message.channel.send("Target index not valid");
+                let targetInvalid = new Discord.RichEmbed()
+                    .setDescription(`<:error:643341473772863508> Sorry, target position #${targetPos + 1} isn't valid`)
+                    .setColor(`#FF0000`)
+                message.channel.send(targetInvalid);
             }
         } else {
-            message.channel.send("Starting index not valid");
+            let startInvalid = new Discord.RichEmbed()
+                .setDescription(`<:error:643341473772863508> Sorry, there isn't a video at position #${startPos + 1}`)
+                .setColor(`#FF0000`)
+            message.channel.send(startInvalid);
         }
     }
 }

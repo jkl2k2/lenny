51207b7c94d22b4e8c55a2ff7bf6ebb8ee2f6cb2
Joshua <joshualomelin@gmail.com>
2020-02-01 15:59:11 -0800

    Added !owo

---
+++ commands/owo.js
@@ -0,0 +1,14 @@
+const index = require(`../index.js`);
+// Any 'require'
+
+module.exports = {
+	name: 'owo',
+	description: 'HUSTLE MY BONES',
+	// aliases: ['aliases'],
+	// usage: '[command]',
+	// cooldown: 5,
+	// guildOnly: true,
+	execute(message, args) {
+		message.channel.send(`Hustle Bones Jul 21, 2018 @ 12:11pm\nJust me and my :two_hearts:daddy:two_hearts:, hanging out I got pretty hungry:eggplant: so I started to pout :disappointed: He asked if I was down :arrow_down:for something yummy :heart_eyes::eggplant: and I asked what and he said he'd give me his :sweat_drops:cummies!:sweat_drops: Yeah! Yeah!:two_hearts::sweat_drops: I drink them!:sweat_drops: I slurp them!:sweat_drops: I swallow them whole:sweat_drops: :heart_eyes: It makes :cupid:daddy:cupid: :blush:happy:blush: so it's my only goal... :two_hearts::sweat_drops::tired_face:Harder daddy! Harder daddy! :tired_face::sweat_drops::two_hearts: 1 cummy:sweat_drops:, 2 cummy:sweat_drops::sweat_drops:, 3 cummy:sweat_drops::sweat_drops::sweat_drops:, 4:sweat_drops::sweat_drops::sweat_drops::sweat_drops: I'm :cupid:daddy's:cupid: :crown:princess :crown:but I'm also a :hearts::hearts::hearts::hearts::hearts:! :heart_decoration: He makes me feel squishy:heartpulse:!He makes me feel good:purple_heart:! :cupid::cupid::cupid:He makes me feel everything a little should!~ :cupid::cupid::cupid: :crown::sweat_drops::cupid:Wa-What!:cupid::sweat_drops::crown:`, {tts : true});
+	}
+}

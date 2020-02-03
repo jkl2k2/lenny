811a21d3f1196e25bb571df0a9e4ecdcf1f2f749
Joshua <joshualomelin@gmail.com>
2020-02-03 00:02:03 -0800

    Updated test and its embed Says if frontend or Discord is lagging

--- commands/ping.js
+++ commands/ping.js
@@ -9,16 +9,29 @@ module.exports = {
 	guildOnly: false,
 	async execute(message, args) {
 		let pingEmbed = new Discord.RichEmbed()
-			.setTitle(`:arrows_counterclockwise: Testing latency...`)
+			.setTitle(` `)
+			.setDescription(`:arrows_counterclockwise: Testing latency...`)
+			.addField(`Frontend Latency`, `Testing...`, true)
+			.addField(`Frontend Status`, `:arrows_counterclockwise: Testing...`, true)
+			.addBlankField()
+			.addField(`Discord API Latency`, `Testing...`, true)
+			.addField(`Discord API Status`, `:arrows_counterclockwise: Testing...`, true)
 			.setColor(`#0083FF`)
 
 		const m = await message.channel.send(pingEmbed);
 
 		let newPingEmbed = new Discord.RichEmbed()
-			.setTitle(`:white_check_mark: Latency Test Completed`)
-			.setColor(`#44C408`)
-			.addField(`Frontend Latency`, `${m.createdTimestamp - message.createdTimestamp}ms`)
-			.addField(`Discord API Latency`, `${Math.round(message.client.ping)}ms`)
+			.setTitle(` `)
+			.setDescription(`:information_source: Latency Test Completed`)
+			.setColor(`#0083FF`);
+		
+		newPingEmbed.addField(`Frontend Latency`, `${m.createdTimestamp - message.createdTimestamp}ms`, true)
+		if ((m.createdTimestamp - message.createdTimestamp) < 300) newPingEmbed.addField(`Frontend Status`, `:white_check_mark: Appears normal`, true);
+		if ((m.createdTimestamp - message.createdTimestamp) >= 300) newPingEmbed.addField(`Frontend Status`, `:warning: Frontend is lagging`, true);
+		newPingEmbed.addBlankField();
+		newPingEmbed.addField(`Discord API Latency`, `${Math.round(message.client.ping)}ms`, true);
+		if (Math.round(message.client.ping) < 100) newPingEmbed.addField(`Discord API Status`, `:white_check_mark: Appears normal`, true);
+		if (Math.round(message.client.ping) >= 100) newPingEmbed.addField(`Discord API Status`, `:warning: Discord is lagging`, true);
 
 		m.edit(newPingEmbed);
 	},

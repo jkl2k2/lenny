6ac1acef217e0430292047d01f0672d77c237a2b
Joshua <joshualomelin@gmail.com>
2020-02-02 23:35:16 -0800

    Updated description

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ const Discord = require(`discord.js`);
 
 module.exports = {
     name: 'help',
-    description: 'List all of my commands or info about a specific command.',
+    description: 'List all bot commands or info about a specific command.',
     aliases: ['commands'],
     usage: '[command name]',
     cooldown: 5,

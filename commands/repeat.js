b01b1334b8603ab71ed1ab1a136df1a4a7009e06
Joshua <joshualomelin@gmail.com>
2020-02-04 21:36:50 -0800

    Added !repeat to loop music

---
+++ commands/repeat.js
@@ -0,0 +1,42 @@
+const index = require(`../index.js`);
+const Discord = require(`discord.js`);
+
+function sendRepeatOn(message) {
+    let repeatOnEmbed = new Discord.RichEmbed()
+        .setDescription(`:repeat: **[${index.getPlayingVideo().getCleanTitle()}](${index.getPlayingVideo().getURL()})** **will** now repeat`)
+        .setColor(`#0083FF`);
+    message.channel.send(repeatOnEmbed);
+}
+
+function sendRepeatOff(message) {
+    let repeatOffEmbed = new Discord.RichEmbed()
+        .setDescription(`:stop_button: **[${index.getPlayingVideo().getCleanTitle()}](${index.getPlayingVideo().getURL()})** will **no longer** repeat`)
+        .setColor(`#0083FF`);
+    message.channel.send(repeatOffEmbed);
+}
+
+module.exports = {
+    name: 'repeat',
+    description: 'Set repeating of current song',
+    // aliases: ['aliases'],
+    usage: '[on/off]',
+    // cooldown: 5,
+    guildOnly: true,
+    execute(message, args) {
+        if (!args[0]) {
+            if (index.getRepeat() == true) {
+                index.setRepeat(false);
+                sendRepeatOff(message);
+            } else if (index.getRepeat() == false) {
+                index.setRepeat(true);
+                sendRepeatOn(message);
+            }
+        } else if (args[0] == "on") {
+            index.setRepeat(true);
+            sendRepeatOn(message);
+        } else if (args[0] == "off") {
+            index.setRepeat(false);
+            sendRepeatOff(message);
+        }
+    }
+}

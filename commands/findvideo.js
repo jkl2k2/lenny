04a371e601c207266b18593cb9587ddab19682f1
Joshua <joshualomelin@gmail.com>
2020-02-20 09:30:11 -0800

    Changed findvideo to use new YT API

--- commands/findvideo.js
+++ commands/findvideo.js
@@ -2,7 +2,7 @@ const index = require(`../index.js`);
 const Discord = require(`discord.js`)
 const config = require('config');
 const api = config.get(`Bot.api`);
-const { YouTube } = require('better-youtube-api');
+const YouTube = require('simple-youtube-api');
 const youtube = new YouTube(api);
 
 module.exports = {
@@ -13,25 +13,24 @@ module.exports = {
     // cooldown: 5,
     guildOnly: true,
     async execute(message, args) {
-        var manualVideoSearch = await youtube.searchVideos(args.join()).catch(function (error) {
-            console.error(error);
-            return;
-        });
-        if (manualVideoSearch[0] != undefined && args.length >= 1) {
-            message.channel.send(`**Best match:**\n${manualVideoSearch[0].url}`);
-        } else if (args.length == 0) {
-            let noArgsSearchEmbed = new Discord.RichEmbed()
-                .setTitle(`:no_entry: **${message.author.username}, you need to provide search terms**`)
-                .setColor(`#FF0000`)
-            message.channel.send(noArgsSearchEmbed);
-        } else {
-            let searchFailEmbed = new Discord.RichEmbed()
-                 
-                .addField(`:no_entry: **Sorry, ${message.author.username}, I couldn't find any videos with those search terms**`, `Please try again with different search terms`)
-                .setTimestamp()
-                .setFooter(`Requested by ${message.author.username}`)
-                .setColor(`#FF0000`)
-            message.channel.send(searchFailEmbed);
-        }
+        youtube.searchVideos(args.join(), 1)
+            .then(videos => {
+                if (videos[0] != undefined && args.length >= 1) {
+                    message.channel.send(`**Best match:**\n${videos[0].url}`);
+                } else if (args.length == 0) {
+                    let noArgsSearchEmbed = new Discord.RichEmbed()
+                        .setTitle(`:no_entry: **${message.author.username}, you need to provide search terms**`)
+                        .setColor(`#FF0000`)
+                    message.channel.send(noArgsSearchEmbed);
+                } else {
+                    let searchFailEmbed = new Discord.RichEmbed()
+
+                        .addField(`:no_entry: **Sorry, ${message.author.username}, I couldn't find any videos with those search terms**`, `Please try again with different search terms`)
+                        .setTimestamp()
+                        .setFooter(`Requested by ${message.author.username}`)
+                        .setColor(`#FF0000`)
+                    message.channel.send(searchFailEmbed);
+                } 
+                });
     }
 }

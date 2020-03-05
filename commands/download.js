23919fe5d5db7953f5c850650ad937164a697792
Joshua <joshualomelin@gmail.com>
2020-03-04 16:38:36 -0800

    Updated download Higher quality video by using ytdl instead of youtube-dl

--- commands/download.js
+++ commands/download.js
@@ -1,9 +1,25 @@
 const index = require(`../index.js`);
+const Discord = require(`discord.js`);
 var fs = require('fs');
 const youtubedl = require('youtube-dl');
 const config = require("config");
 const ownerID = config.get("Users.ownerID");
 const logger = index.getLogger();
+
+const readline = require(`readline`);
+const path = require(`path`);
+const ffmpeg = require(`fluent-ffmpeg`);
+const ytdl = require(`ytdl-core`);
+
+const audioOutput = path.resolve(`F:\\Bot\\L-Bot-Rewrite\\downloads`, 'sound.mp4');
+const mainOutput = path.resolve(`F:\\Bot\\L-Bot-Rewrite\\downloads`, 'output.mp4');
+
+const onProgress = (chunkLength, downloaded, total) => {
+    const percent = downloaded / total;
+    readline.cursorTo(process.stdout, 0);
+    process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
+    process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)`);
+};
 
 module.exports = {
     name: 'download',
@@ -19,12 +35,47 @@ module.exports = {
 
         var video;
 
-        if(args[1]) {
-            video = youtubedl(args[1], ["-f 140"]);
+        if (args[0] == "audio") {
+            // video = youtubedl(args[1], ["-f 140"]);
+            ytdl(args[1], {
+                quality: "highestaudio",
+                highWaterMark: 1 << 25 
+            }).on('error', logger.error)
+                .on('progress', onProgress)
+                .pipe(fs.createWriteStream(audioOutput));
         } else {
-            video = youtubedl(args[0]);
+            console.log('Downloading audio track');
+
+            ytdl(args[0], {
+                // filter: format => format.container === 'mp4' && !format.qualityLabel,
+                quality: "highestaudio"
+            }).on('error', console.error)
+                .on('progress', onProgress)
+                .pipe(fs.createWriteStream(audioOutput))
+                .on('finish', () => {
+                    console.log('\nDownloading video');
+                    const video = ytdl(args[0], {
+                        // filter: format => format.container === 'mp4' && !format.audioEncoding,
+                        quality: 137
+                    });
+                    video.on('progress', onProgress);
+                    ffmpeg()
+                        .input(video)
+                        .videoCodec('copy')
+                        .input(audioOutput)
+                        .audioCodec('copy')
+                        .save(mainOutput)
+                        .on('error', console.error)
+                        .on('end', () => {
+                            fs.unlink(audioOutput, err => {
+                                if (err) console.error(err);
+                                else console.log('\nFinished downloading, saved to ' + mainOutput);
+                            });
+                        });
+                });
         }
 
+        /*
         video.on('info', function (info) {
             logger.info('Download started');
             logger.info('filename: ' + info._filename);
@@ -32,5 +83,6 @@ module.exports = {
 
             video.pipe(fs.createWriteStream(`./downloads/${info._filename}`));
         });
+        */
     }
 }

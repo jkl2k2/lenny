f3749757975e225b32214c08d6387fa1d79aa184
Joshua <joshualomelin@gmail.com>
2020-03-13 17:44:42 -0700

    Download sends the file in chat

--- commands/download.js
+++ commands/download.js
@@ -13,13 +13,7 @@ const ytdl = require(`ytdl-core`);
 
 const audioOutput = path.resolve(`F:\\Bot\\L-Bot-Rewrite\\downloads`, 'sound.mp4');
 const mainOutput = path.resolve(`F:\\Bot\\L-Bot-Rewrite\\downloads`, 'output.mp4');
-
-const onProgress = (chunkLength, downloaded, total) => {
-    const percent = downloaded / total;
-    readline.cursorTo(process.stdout, 0);
-    process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
-    process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)`);
-};
+const secondaryAudioOutput = path.resolve(`F:\\Bot\\L-Bot-Rewrite\\downloads`, 'audio.mp4');
 
 module.exports = {
     name: 'download',
@@ -28,23 +22,46 @@ module.exports = {
     usage: '[url]',
     // cooldown: 5,
     // guildOnly: true,
-    execute(message, args) {
-        if(message.author.id != ownerID) {
+    async execute(message, args) {
+        if (message.author.id != ownerID) {
             return;
         }
+
+        var progressEmbed = new Discord.RichEmbed()
+            .setDescription(`:arrows_counterclockwise: Downloading audio`)
+            .setColor(`#0083FF`)
+        var progressMsg = await message.channel.send(progressEmbed);
+
+        const onProgress = (chunkLength, downloaded, total) => {
+            const percent = downloaded / total;
+            readline.cursorTo(process.stdout, 0);
+            process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
+            process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)`);
+            if ((downloaded / total) == 1) {
+                // When 100% downloaded
+                let progressEmbed = new Discord.RichEmbed()
+                    .setDescription(`:arrow_double_up: Uploading file...`)
+                    .setColor(`#0083FF`)
+                progressMsg.edit(progressEmbed);
+                setTimeout(() => {
+                    message.channel.send(`File:`, { files: [`./downloads/audio.mp4`] });
+                }, 1000);
+            }
+        };
 
         var video;
 
         if (args[0] == "audio") {
             // video = youtubedl(args[1], ["-f 140"]);
             ytdl(args[1], {
-                quality: "highestaudio",
-                highWaterMark: 1 << 25 
+                filter: format => format.container === 'mp4' && !format.qualityLabel,
+                // quality: "highestaudio",
+                highWaterMark: 1 << 25
             }).on('error', logger.error)
                 .on('progress', onProgress)
-                .pipe(fs.createWriteStream(audioOutput));
-        } else {
-            console.log('Downloading audio track');
+                .pipe(fs.createWriteStream(secondaryAudioOutput))
+        } else if (args[0] == "video") {
+            logger.debug('Downloading audio track');
 
             ytdl(args[0], {
                 // filter: format => format.container === 'mp4' && !format.qualityLabel,
@@ -53,7 +70,7 @@ module.exports = {
                 .on('progress', onProgress)
                 .pipe(fs.createWriteStream(audioOutput))
                 .on('finish', () => {
-                    console.log('\nDownloading video');
+                    console.log('\nDownloading video...');
                     const video = ytdl(args[0], {
                         // filter: format => format.container === 'mp4' && !format.audioEncoding,
                         quality: 137
@@ -68,8 +85,8 @@ module.exports = {
                         .on('error', console.error)
                         .on('end', () => {
                             fs.unlink(audioOutput, err => {
-                                if (err) console.error(err);
-                                else console.log('\nFinished downloading, saved to ' + mainOutput);
+                                if (err) logger.error(err);
+                                else logger.info('\nFinished downloading, saved to ' + mainOutput);
                             });
                         });
                 });

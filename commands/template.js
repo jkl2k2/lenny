477810e205838eeffde3d1ecc23af12d54954509
Joshua <joshualomelin@gmail.com>
2020-03-13 17:42:44 -0700

    Added args field to command template for future use

--- commands/template.js
+++ commands/template.js
@@ -5,6 +5,7 @@ module.exports = {
 	name: 'template',
 	description: 'Template command for easier coding, does nothing',
 	// aliases: ['aliases'],
+	// args: true,
 	// usage: '[command]',
 	// cooldown: 5,
 	// guildOnly: true,

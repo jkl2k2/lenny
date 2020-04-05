71f67e63281766aa644d14fcc46d1d4778035077
Joshua <joshualomelin@gmail.com>
2020-04-05 00:53:06 -0700

    Installed Sequelize and sqlite3, set up database

---
+++ dbInit.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'database.sqlite',
+});
+
+const CurrencyShop = sequelize.import('models/CurrencyShop');
+sequelize.import('models/Users');
+sequelize.import('models/UserItems');
+
+const force = process.argv.includes('--force') || process.argv.includes('-f');
+
+sequelize.sync({ force }).then(async () => {
+    const shop = [
+        CurrencyShop.upsert({ name: 'Tea', cost: 1 }),
+        CurrencyShop.upsert({ name: 'Coffee', cost: 2 }),
+        CurrencyShop.upsert({ name: 'Cake', cost: 5 }),
+    ];
+    await Promise.all(shop);
+    console.log('Database synced');
+    sequelize.close();
+}).catch(console.error);

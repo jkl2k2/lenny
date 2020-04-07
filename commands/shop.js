bfe08f701101a714e5928dc48929759dfd88eb3d
Joshua <joshualomelin@gmail.com>
2020-04-07 00:46:20 -0700

    Added !shop

---
+++ commands/shop.js
@@ -0,0 +1,19 @@
+const index = require(`../index.js`);
+const { Users, CurrencyShop } = require('../dbObjects');
+const { Op } = require('sequelize');
+const currency = index.getCurrencyDB();
+
+module.exports = {
+    name: 'shop',
+    description: 'Template command for easier coding, does nothing',
+    // aliases: ['aliases'],
+    // args: true,
+    // usage: '[command]',
+    // cooldown: 5,
+    // guildOnly: true,
+    enabled: true,
+    async execute(message, args) {
+        const items = await CurrencyShop.findAll();
+        return message.channel.send(items.map(item => `${item.name}: ${item.cost}💰`).join('\n'), { code: true });
+    }
+};

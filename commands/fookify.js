036213729f4ed5c198127883944a4fff98b18348
Joshua <joshualomelin@gmail.com>
2020-04-22 19:40:58 -0700

    Added fookify and fookifytts

---
+++ commands/fookify.js
@@ -0,0 +1,18 @@
+const index = require(`../index.js`);
+const Discord = require(`discord.js`);
+// Any 'require'
+
+module.exports = {
+    name: 'fookify',
+    description: 'Fookify your input',
+    // aliases: ['aliases'],
+    args: true,
+    usage: '[text]',
+    // altUsage: 'command',
+    cooldown: 1,
+    // guildOnly: true,
+    enabled: true,
+    execute(message, args) {
+        message.channel.send(args.join(`\n`));
+    }
+};

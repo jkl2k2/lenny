8baf463fdfa34a47b3ed302f35078311ba43b7da
Joshua <joshualomelin@gmail.com>
2020-04-24 11:49:12 -0700

    Added !corn

---
+++ commands/corn.js
@@ -0,0 +1,83 @@
+const index = require(`../index.js`);
+const Discord = require(`discord.js`);
+const config = require(`config`);
+const CSE_ID = config.get(`Bot.CSE_ID`);
+const api = config.get(`Bot.api`);
+const GoogleImages = require(`google-images`);
+const images = new GoogleImages(CSE_ID, api);
+
+module.exports = {
+    name: 'corn',
+    description: 'Displays a random picture of corn',
+    // aliases: ['aliases'],
+    // args: true,
+    // usage: '[command]',
+    // altUsage: 'command',
+    // cooldown: 5,
+    // guildOnly: true,
+    enabled: true,
+    async execute(message, args) {
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * Math.floor(max));
+        }
+
+        var queryChoice = getRandomInt(6);
+        var pictureChoice = getRandomInt(10);
+
+        switch (queryChoice) {
+            default:
+                images.search('corn')
+                    .then(images => {
+                        message.channel.send(new Discord.RichEmbed()
+                            .setImage(images[pictureChoice].url)
+                            .setColor(`#FCF403`)
+                            .setDescription(`OwO what's this?`));
+                    });
+                break;
+            case 1:
+                images.search('buttery corn')
+                    .then(images => {
+                        message.channel.send(new Discord.RichEmbed()
+                            .setImage(images[pictureChoice].url)
+                            .setColor(`#FCF403`)
+                            .setDescription(`OwO what's this?`));
+                    });
+                break;
+            case 2:
+                images.search('steamy corn')
+                    .then(images => {
+                        message.channel.send(new Discord.RichEmbed()
+                            .setImage(images[pictureChoice].url)
+                            .setColor(`#FCF403`)
+                            .setDescription(`OwO what's this?`));
+                    });
+                break;
+            case 3:
+                images.search('delicious corn')
+                    .then(images => {
+                        message.channel.send(new Discord.RichEmbed()
+                            .setImage(images[pictureChoice].url)
+                            .setColor(`#FCF403`)
+                            .setDescription(`OwO what's this?`));
+                    });
+                break;
+            case 4:
+                images.search('HD corn')
+                    .then(images => {
+                        message.channel.send(new Discord.RichEmbed()
+                            .setImage(images[pictureChoice].url)
+                            .setColor(`#FCF403`)
+                            .setDescription(`OwO what's this?`));
+                    });
+                break;
+            case 5:
+                images.search('fresh and steamy corn')
+                    .then(images => {
+                        message.channel.send(new Discord.RichEmbed()
+                            .setImage(images[pictureChoice].url)
+                            .setColor(`#FCF403`)
+                            .setDescription(`OwO what's this?`));
+                    });
+        }
+    }
+};

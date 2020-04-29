bf8bc5352de51eff8aa1af75961c629c8f40ee12
Joshua <joshualomelin@gmail.com>
2020-04-29 16:47:11 -0700

    Code cleanup and embed update

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -13,69 +13,49 @@ module.exports = {
     enabled: true,
     type: 'music',
     execute(message, args) {
-        args.unshift();
-        // index.removeFromQueue(args[0]);
-
         var queue = index.getQueue(message);
 
         var target = parseInt(args[0]);
 
         var splitArgs = args[0].split('-');
 
-        logger.debug(splitArgs);
-
         if (queue == undefined || queue[target - 1] == undefined) {
-            let indexDNEEmbed = new Discord.RichEmbed()
-
+            // Invalid target
+            return message.channel.send(new Discord.RichEmbed()
                 .setDescription(`<:error:643341473772863508> ${target} is not a valid position in the queue`)
-                .setColor(`#FF0000`);
-            message.channel.send(indexDNEEmbed);
-            return;
+                .setColor(`#FF0000`));
         }
 
         if (splitArgs.length == 2) {
-            // console.log(`splitArgs is exactly 2 entries long`);
-
+            // If range
             var index1 = parseInt(splitArgs[0]) - 1;
             var index2 = parseInt(splitArgs[1]) - 1;
 
             if (queue[index1] && queue[index2]) {
-                // message.channel.send(`Remove range is valid for start index [${index1}] and end index [${index2}]`);
-                let removeRangeEmbed = new Discord.RichEmbed()
-
-                    .setDescription(`:eject: Removed videos from\n[${queue[index1].getTitle()}](${queue[index1].getURL()})\nup to\n[${queue[index2].getTitle()}](${queue[index2].getURL()})`)
-                    .setColor(`#0083FF`);
-                message.channel.send(removeRangeEmbed);
+                // Range remove successful
                 queue.splice(index1, (index2 - index1) + 1);
 
-                return;
+                return message.channel.send(new Discord.RichEmbed()
+                    .setDescription(`:eject: Removed\n**[${queue[index1].getTitle()}](${queue[index1].getURL()})**\n[+${index2 - index1 - 1} other video(s)] up to\n**[${queue[index2].getTitle()}](${queue[index2].getURL()})**`)
+                    .setColor(`#0083FF`));
             } else {
-                // message.channel.send(`Remove range is NOT valid for start index [${index1}] and end index [${index2}]`);
-                let invalidRemoveEmbed = new Discord.RichEmbed()
-
+                // Invalid range
+                return message.channel.send(new Discord.RichEmbed()
                     .setDescription(`<:error:643341473772863508> The range you provided is not valid`)
-                    .setColor(`#FF0000`);
-                message.channel.send(invalidRemoveEmbed);
-
-                return;
+                    .setColor(`#FF0000`));
             }
         }
 
         var elementToRemove = queue[target - 1];
         queue.splice(target - 1, 1);
         if (elementToRemove != queue[target]) {
-            let queueRemoveEmbed = new Discord.RichEmbed()
-
-                .setDescription(`:eject: ${message.author.username} removed\n**[${elementToRemove.getTitle()}](${elementToRemove.getURL()})**\nfrom queue`)
-                .setColor(`#0083FF`);
-            message.channel.send(queueRemoveEmbed);
-            // message.reply(`successfully removed "${elementToRemove.videoTitle}" from queue!`);
+            message.channel.send(new Discord.RichEmbed()
+                .setDescription(`:eject: ${message.author.username} removed **[${elementToRemove.getTitle()}](${elementToRemove.getURL()})**`)
+                .setColor(`#0083FF`));
         } else {
-            let queueRemoveEmbed = new Discord.RichEmbed()
+            message.channel.send(new Discord.RichEmbed()
                 .setTitle(`Somehow, I failed to remove "[${elementToRemove.getTitle()}](${elementToRemove.getURL()})" from queue. This should never happen.`)
-                .setColor(`#FF0000`);
-            message.channel.send(queueRemoveEmbed);
-            // message.reply(`:thinking: I don't understand.`);
+                .setColor(`#FF0000`));
         }
     }
 };

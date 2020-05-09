5d39443af12066d3e411e5ac43637edc5a65effb
Joshua <joshualomelin@gmail.com>
2020-05-08 23:10:39 -0700

    Added !clear, which clears queue without stopping playback

---
+++ commands/music/clear.js
@@ -0,0 +1,30 @@
+const index = require(`../../index.js`);
+const Discord = require(`discord.js`);
+// Any 'require'
+
+module.exports = {
+    name: 'clear',
+    description: 'Clears the queue without stopping playback',
+    // aliases: ['aliases'],
+    // args: true,
+    // usage: '[command]',
+    // altUsage: 'command',
+    // cooldown: 5,
+    guildOnly: true,
+    enabled: true,
+    type: 'music',
+    execute(message, args) {
+        var queue = index.getQueue(message);
+
+        if (queue == undefined) {
+            return message.channel.send(new Discord.RichEmbed()
+                .setDescription(`<:error:643341473772863508> There is nothing to skip`)
+                .setColor(`#FF0000`));
+        } else {
+            index.setQueue(message, []);
+            return message.channel.send(new Discord.RichEmbed()
+                .setDescription(`:arrow_double_up: ${message.author.username} cleared the queue`)
+                .setColor(`#0083FF`));
+        }
+    }
+};

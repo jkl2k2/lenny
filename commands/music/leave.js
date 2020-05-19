d33d70d5996cd5365e3b41f8a3855b51b107ff56
Joshua <joshualomelin@gmail.com>
2020-05-18 23:31:21 -0700

    Made voice channel name between single ticks

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -14,13 +14,12 @@ module.exports = {
 	execute(message, args) {
 		if (client.voiceConnections.get(message.guild.id) != undefined) {
 			message.channel.send(new Discord.RichEmbed()
-				.setDescription(`:arrow_left: Disconnected from ${client.voiceConnections.get(message.guild.id).channel.name}`)
+				.setDescription(`:arrow_left: Disconnected from \`${client.voiceConnections.get(message.guild.id).channel.name}\``)
 				.setColor(`#0083FF`));
 
 			client.voiceConnections.get(message.guild.id).disconnect();
 		} else {
 			message.channel.send(new Discord.RichEmbed()
-
 				.setDescription(`<:error:643341473772863508> I'm not in a voice channel`)
 				.setColor(`#FF0000`));
 		}

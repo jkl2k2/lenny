488dc188721a637c0a4ff3e370f9b2f83807b1a5
Joshua <joshualomelin@gmail.com>
2020-05-31 17:35:51 -0700

    Removed home button from queue reactions Code cleanup

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -38,20 +38,27 @@ async function reactionHandler(sent, message, page) {
 	};
 
 	if (page == 0 && !queue[5]) {
-		// sent.react('🔘');
+
+		// Only first page exists
+
 	} else if (!queue[(page + 1) * 5]) {
+
+		// Last page
 		sent.react('◀️')
-			.then(() => (sent.react('🔘'))
-				//.then(() => sent.react('▶️'))
-				.catch(() => console.error('One of the emojis failed to react.')));
+			.catch(() => console.error('One of the emojis failed to react.'));
+
 	} else if (page == 0) {
-		sent.react('🔘')
-			.then(() => (sent.react('▶️')));
+
+		// First page
+		sent.react('▶️');
+
 	} else if (page > 0) {
+
+		// Any page between first and last
 		sent.react('◀️')
-			.then(() => (sent.react('🔘'))
-				.then(() => sent.react('▶️'))
-				.catch(() => console.error('One of the emojis failed to react.')));
+			.then(() => sent.react('▶️'))
+			.catch(() => console.error('One of the emojis failed to react.'));
+
 	}
 
 	sent.awaitReactions(filter, { max: 1, time: 30000, errors: ['time'] })
@@ -59,38 +66,34 @@ async function reactionHandler(sent, message, page) {
 			const reaction = collected.first();
 
 			if (reaction.emoji.name === '◀️') {
-				//message.channel.send('(This would go back a page)');
+				// Previous page
 				sent.delete();
-				let newEmbed = new Discord.RichEmbed()
-					.setTitle(`(What would be page ${page - 1})`);
 				let newSent = await sendEmbed(page - 1, message);
 				reactionHandler(newSent, message, page - 1);
 			} else if (reaction.emoji.name === "🔘") {
-				//message.channel.send('(This would go to page 1)');
+				// Home page (first page)
 				sent.delete();
-				let newEmbed = new Discord.RichEmbed()
-					.setTitle(`(What would be page ${0})`);
 				let newSent = await sendEmbed(0, message);
 				reactionHandler(newSent, message, 0);
 			} else if (reaction.emoji.name === "▶️") {
-				//message.channel.send('(This would go to the next page)');
+				// Next page
 				sent.delete();
-				let newEmbed = new Discord.RichEmbed()
-					.setTitle(`(What would be page ${page + 1})`);
 				let newSent = await sendEmbed(page + 1, message);
 				reactionHandler(newSent, message, page + 1);
 			}
 		})
 		.catch(async collected => {
 			// message.reply('Reaction timeout');
+			/*
 			let noControlQueue = new Discord.RichEmbed()
 
 				// .setDescription(`${queueResolver(parsedQueue, 0)}\n\n${queueResolver(parsedQueue, 1)}\n\n${queueResolver(parsedQueue, 2)}\n\n${queueResolver(parsedQueue, 3)}\n\n${queueResolver(parsedQueue, 4)}\n\n${queueOverflowResolver(parsedQueue)}`)
 				.setDescription(`${await queueResolver(queue, 0 + page * 5)}\n\n${await queueResolver(queue, 1 + page * 5)}\n\n${await queueResolver(queue, 2 + page * 5)}\n\n${await queueResolver(queue, 3 + page * 5)}\n\n${await queueResolver(queue, 4 + page * 5)}\n\n${queueOverflowResolver(queue)}`)
 				.setAuthor(`Current queue - Page ${page + 1}`, message.guild.iconURL)
 				.setColor(`#0083FF`);
+			*/
 			// .setFooter(`Controls cleared due to inactivity`);
-			sent.edit(noControlQueue);
+			// sent.edit(noControlQueue);
 			sent.clearReactions();
 		});
 }

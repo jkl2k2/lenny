4e069ecb0db972bccbf22117b179543bce527c39
Joshua <joshualomelin@gmail.com>
2020-06-17 23:56:41 -0700

    !leave now also resets dispatcher and queue

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -4,20 +4,26 @@ var client = index.getClient();
 
 module.exports = {
 	name: 'leave',
-	description: `Leaves the user's voice channel`,
+	description: `Leaves the user's voice channel, clearing the queue and anything that was playing`,
 	aliases: ['l'],
 	// usage: '[usage]',
 	// cooldown: seconds,
 	guildOnly: true,
 	enabled: true,
 	type: 'music',
-	execute(message, args) {
+	async execute(message, args) {
 		if (client.voiceConnections.get(message.guild.id) != undefined) {
-			message.channel.send(new Discord.RichEmbed()
-				.setDescription(`:arrow_left: Disconnected from \`${client.voiceConnections.get(message.guild.id).channel.name}\``)
+			let channelName = client.voiceConnections.get(message.guild.id).channel.name;
+			let disconnecting = await message.channel.send(new Discord.RichEmbed()
+				.setDescription(`:arrows_counterclockwise: Disconnecting from \`${channelName}`)
 				.setColor(`#0083FF`));
-
+			index.setDispatcher(message, undefined);
+			index.setQueue(message, index.constructQueue());
 			client.voiceConnections.get(message.guild.id).disconnect();
+
+			disconnecting.edit(new Discord.RichEmbed()
+				.setDescription(`:arrow_left: Disconnected from \`${channelName}\``)
+				.setColor(`#0083FF`));
 		} else {
 			message.channel.send(new Discord.RichEmbed()
 				.setDescription(`<:error:643341473772863508> I'm not in a voice channel`)

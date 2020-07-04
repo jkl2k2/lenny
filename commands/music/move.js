2400171289048fb043950ce80e62ac72bd9a71be
Joshua <joshualomelin@gmail.com>
2020-07-04 01:25:23 -0700

    Fixed !move causing an error if queue is undefined Updated check and cross emojis

--- commands/music/move.js
+++ commands/music/move.js
@@ -13,27 +13,36 @@ module.exports = {
     enabled: true,
     type: 'music',
     execute(message, args) {
-        var queue = index.getQueue(message).list;
+        var fullQueue = index.getQueue(message);
+        var queue;
         var startPos = args[0] - 1;
         var targetPos = args[1] - 1;
 
+        if (fullQueue == undefined) {
+            return message.channel.send(new Discord.RichEmbed()
+                .setDescription(`<:cross:728885860623319120> The queue is empty`)
+                .setColor(`#FF0000`));
+        } else {
+            queue = fullQueue.list;
+        }
+
         if (!args[0]) {
             return message.channel.send(new Discord.RichEmbed()
-                .setDescription(`<:error:643341473772863508> Please specify a start position`)
+                .setDescription(`<:cross:728885860623319120> Please specify a start position`)
                 .setColor(`#FF0000`));
         } else if (!args[1]) {
             return message.channel.send(new Discord.RichEmbed()
-                .setDescription(`<:error:643341473772863508> Please specify a target position`)
+                .setDescription(`<:cross:728885860623319120> Please specify a target position`)
                 .setColor(`#FF0000`));
         }
 
         if (isNaN(args[0])) {
             return message.channel.send(new Discord.RichEmbed()
-                .setDescription(`<:error:643341473772863508> Start position must be a number`)
+                .setDescription(`<:cross:728885860623319120> Start position must be a number`)
                 .setColor(`#FF0000`));
         } else if (isNaN(args[1])) {
             return message.channel.send(new Discord.RichEmbed()
-                .setDescription(`<:error:643341473772863508> Target position must be a number`)
+                .setDescription(`<:cross:728885860623319120> Target position must be a number`)
                 .setColor(`#FF0000`));
         }
 
@@ -44,27 +53,27 @@ module.exports = {
                 queue.splice(targetPos, 0, queue.splice(startPos, 1)[0]);
                 if (targetPos == 0) {
                     message.channel.send(new Discord.RichEmbed()
-                        .setDescription(`:white_check_mark: Moved from position #${startPos + 1} to #${targetPos + 1}:\n**[${queue[targetPos].getCleanTitle()}](${queue[targetPos].getURL()})**
+                        .setDescription(`<:check:728881238970073090> Moved from position #${startPos + 1} to #${targetPos + 1}:\n**[${queue[targetPos].getCleanTitle()}](${queue[targetPos].getURL()})**
 
                                          It will now play after:
                                          **[${index.getQueue(message).lastPlayed.getCleanTitle()}](${index.getQueue(message).lastPlayed.getURL()})**`)
-                        .setColor(`#44C408`));
+                        .setColor(`#2EC14E`));
                 } else {
                     message.channel.send(new Discord.RichEmbed()
-                        .setDescription(`:white_check_mark: Moved from position #${startPos + 1} to #${targetPos + 1}:\n**[${queue[targetPos].getCleanTitle()}](${queue[targetPos].getURL()})**
+                        .setDescription(`<:check:728881238970073090> Moved from position #${startPos + 1} to #${targetPos + 1}:\n**[${queue[targetPos].getCleanTitle()}](${queue[targetPos].getURL()})**
 
                                          It will now play after:
                                          **[${queue[targetPos - 1].getCleanTitle()}](${queue[targetPos - 1].getURL()})**`)
-                        .setColor(`#44C408`));
+                        .setColor(`#2EC14E`));
                 }
             } else {
                 message.channel.send(new Discord.RichEmbed()
-                    .setDescription(`<:error:643341473772863508> Sorry, target position #${targetPos + 1} isn't valid`)
+                    .setDescription(`<:cross:728885860623319120> Sorry, target position #${targetPos + 1} isn't valid`)
                     .setColor(`#FF0000`));
             }
         } else {
             message.channel.send(new Discord.RichEmbed()
-                .setDescription(`<:error:643341473772863508> Sorry, there isn't a video at position #${startPos + 1}`)
+                .setDescription(`<:cross:728885860623319120> Sorry, there isn't a video at position #${startPos + 1}`)
                 .setColor(`#FF0000`));
         }
     }

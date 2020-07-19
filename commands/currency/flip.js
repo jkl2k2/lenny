382712b5671f9204c6d4fa042f2cb1082319349e
Joshua <joshualomelin@gmail.com>
2020-07-19 15:49:59 -0700

    Added support for betting on a specific coin side

--- commands/currency/flip.js
+++ commands/currency/flip.js
@@ -11,7 +11,8 @@ module.exports = {
     description: 'A simple coin flip: Heads to win double your bet, tails to lose your bet.',
     aliases: ['coin', 'coinflip', 'flipcoin'],
     args: true,
-    usage: '[amount to bet]',
+    usage: '[h/t] [bet] --or-- [bet] [h/t]',
+    altUsage: '[bet]',
     cooldown: 2,
     // guildOnly: true,
     enabled: true,
@@ -23,68 +24,130 @@ module.exports = {
 
         var originalBalance = currency.getBalance(message.author.id);
 
-        if (args[0] == "coosin" || args[0] == "collin" || args[0] == "cucino") {
+        let bet;
+        let sideArg;
+        let side;
+
+        if (isNaN(args[0]) && !isNaN(args[1])) {
+            // if second argument is bet
+            bet = args[1];
+            sideArg = args[0];
+        } else if (!isNaN(args[0]) && isNaN(args[1])) {
+            // if first argument is bet
+            bet = args[0];
+            sideArg = args[1];
+        } else {
+            // catch
+            bet = "catch";
+        }
+
+        if (sideArg && (sideArg.toLowerCase() == "heads" || sideArg.toLowerCase() == "h")) {
+            // if heads provided
+            side = "heads";
+        } else if (sideArg && (sideArg.toLowerCase() == "tails" || sideArg.toLowerCase() == "t")) {
+            // if tails provided
+            side = "tails";
+        } else {
+            // default to heads
+            side = "heads";
+        }
+
+        if (bet == "coosin" || bet == "collin" || bet == "cucino") {
             return message.channel.send(new Discord.MessageEmbed()
                 .setDescription(`<:cross:729019052571492434> Sorry, we do not accept trash as a currency for betting.`)
                 .setColor(`#FF3838`));
         }
 
-        if (isNaN(args[0])) {
+        if (isNaN(bet)) {
             return message.channel.send(new Discord.MessageEmbed()
                 .setDescription(`<:cross:729019052571492434> Please input a number to bet`)
                 .setColor(`#FF3838`));
         }
 
-        if (parseInt(args[0]) == 0) {
+        if (parseInt(bet) == 0) {
             return message.channel.send(new Discord.MessageEmbed()
                 .setDescription(`<:cross:729019052571492434> Please bet more than $0`)
                 .setColor(`#FF3838`));
         }
 
-        if (parseInt((args[0]) < 1 && parseInt(args[0]) > 0) || (Math.floor(args[0]) != args[0])) {
+        if (parseInt((bet) < 1 && parseInt(bet) > 0) || (Math.floor(bet) != bet)) {
             return message.channel.send(new Discord.MessageEmbed()
                 .setDescription(`<:cross:729019052571492434> Please input a whole number, not a decimal`)
                 .setColor(`#FF3838`));
         }
 
-        if (parseInt(args[0]) < 0) {
+        if (parseInt(bet) < 0) {
             return message.channel.send(new Discord.MessageEmbed()
                 .setDescription(`<:cross:729019052571492434> Please input a positive number`)
                 .setColor(`#FF3838`));
         }
 
-        if (originalBalance < args[0]) {
+        if (originalBalance < bet) {
             return message.channel.send(new Discord.MessageEmbed()
                 .setDescription("<:cross:729019052571492434> Sorry, you do not have enough money to bet that amount")
                 .setColor(`#FF3838`));
         }
 
+        // 1 is heads
+        // 2 is tails
         if (rand == 1) {
-            // Loss
+            // Heads
+            if (side == "heads") {
+                // Win
 
-            // Deduct loss from account
-            currency.add(message.author.id, -parseInt(args[0]));
+                // Add winnings to account
+                currency.add(message.author.id, parseInt(bet));
 
-            // Add to casino profits
-            currency.add("0", parseInt(args[0]));
+                // Deduct from casino profits
+                currency.add("0", -parseInt(bet));
 
-            // Loss message
-            message.channel.send(new Discord.MessageEmbed()
-                .setDescription(`:game_die: You flipped: \`Tails\`\n\nSorry, ${message.author.username}! You **lost** your bet of **$${args[0]}**.\n\nPrevious balance: **$${originalBalance}**\nNew balance: **$${currency.getBalance(message.author.id)}**`)
-                .setColor(`#801431`));
+                // Win message
+                message.channel.send(new Discord.MessageEmbed()
+                    .setDescription(`:game_die: You flipped: \`Heads\`\n\nCongrats, ${message.author.username}! You **won** your bet of **$${bet}**!\n\nPrevious balance: **$${originalBalance}**\nNew balance: **$${currency.getBalance(message.author.id)}**`)
+                    .setColor(`#801431`));
+            } else {
+                // Loss
+
+                // Deduct loss from account
+                currency.add(message.author.id, -parseInt(bet));
+
+                // Add to casino profits
+                currency.add("0", parseInt(bet));
+
+                // Loss message
+                message.channel.send(new Discord.MessageEmbed()
+                    .setDescription(`:game_die: You flipped: \`Heads\`\n\nSorry, ${message.author.username}! You **lost** your bet of **$${bet}**.\n\nPrevious balance: **$${originalBalance}**\nNew balance: **$${currency.getBalance(message.author.id)}**`)
+                    .setColor(`#801431`));
+            }
         } else {
-            // Win
+            // Tails
+            if (side == "heads") {
+                // Loss
 
-            // Add winnings to account
-            currency.add(message.author.id, parseInt(args[0]));
+                // Deduct loss from account
+                currency.add(message.author.id, -parseInt(bet));
 
-            // Deduct from casino profits
-            currency.add("0", -parseInt(args[0]));
+                // Add to casino profits
+                currency.add("0", parseInt(bet));
 
-            // Win message
-            message.channel.send(new Discord.MessageEmbed()
-                .setDescription(`:game_die: You flipped: \`Heads\`\n\nCongrats, ${message.author.username}! You **won** your bet of **$${args[0]}**!\n\nPrevious balance: **$${originalBalance}**\nNew balance: **$${currency.getBalance(message.author.id)}**`)
-                .setColor(`#801431`));
+                // Loss message
+                message.channel.send(new Discord.MessageEmbed()
+                    .setDescription(`:game_die: You flipped: \`Tails\`\n\nSorry, ${message.author.username}! You **lost** your bet of **$${bet}**.\n\nPrevious balance: **$${originalBalance}**\nNew balance: **$${currency.getBalance(message.author.id)}**`)
+                    .setColor(`#801431`));
+            } else {
+                // Win
+
+                // Add winnings to account
+                currency.add(message.author.id, parseInt(bet));
+
+                // Deduct from casino profits
+                currency.add("0", -parseInt(bet));
+
+                // Win message
+                message.channel.send(new Discord.MessageEmbed()
+                    .setDescription(`:game_die: You flipped: \`Tails\`\n\nCongrats, ${message.author.username}! You **won** your bet of **$${bet}**!\n\nPrevious balance: **$${originalBalance}**\nNew balance: **$${currency.getBalance(message.author.id)}**`)
+                    .setColor(`#801431`));
+            }
         }
     }
 };

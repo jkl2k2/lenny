100046f11183bbe5dde073ed5bd47edf77ae8774
Joshua <joshualomelin@gmail.com>
2020-08-03 00:09:08 -0700

    Made ban and kick commands serious

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -5,7 +5,7 @@ const config = require(`config`);
 
 module.exports = {
     name: 'ban',
-    description: 'Bans a user from the server and DMs them a glorious paragraph',
+    description: 'Bans a user from the server',
     // aliases: ['aliases'],
     args: true,
     usage: '[user to kick]',
@@ -18,19 +18,22 @@ module.exports = {
     },
     type: 'admin',
     execute(message, args) {
-        const theMessage = `I only wish to rights wrongs and seek justice, no matter what. I will do what needs to be done to make sure this place experiences a change, a good change. Im not a hero, Im just someone who wants the world to be a better place, whatever it takes. Even if I have to sacrifice myself in the process. I'll be okay knowing what I did what's right, because for me, its not about winning, its not cause I want to beat someone, to kill someone, to make them feel bad, and its not cause it works because it rarely ever does, I do what I'm doing because its right, because its decent, and above all, its kind. That's it, its just kind..... But maybe this won't work, maybe more things will happen, y'know, maybe there's no point in doing this, but I cannot allow myself to stand by and watch as others suffer. Its the best I can do, so Im going to do it. I will stand right here, right now, because I believe in this cause, please, stand with me. At least give it a try, give it a chance, regardless if this works, because at least, I can say I tried to make a different. All I need is for you to lend me a hand, Mods. I believe, deep within my heart, that Life Will Change. I'll wait for your decision. Feel free to dm me about this. And perhaps soon, this'll all be behind us. I do not regret my choice.`;
-
         const target = message.mentions.members.first();
-
-        target.send(theMessage)
-            .then(() => {
-                // var kickTarget = client.users.get(target);
-                message.channel.send(`User has been banned.`);
-                return target.ban("User banned with the !ban command");
-            })
-            .catch(error => {
-                console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                message.channel.send("User has DMs disabled, unable to send message");
-            });
+        args.shift();
+        if (args.length > 0) {
+            return target.ban({ reason: `User banned by ${message.author.tag} for reason: "${args.join(" ")}"` })
+                .catch(error => {
+                    message.channel.send(new Discord.MessageEmbed()
+                        .setDescription(`<:cross:729019052571492434> Ban failed: \`${error}\``)
+                        .setColor(`#FF3838`));
+                });
+        } else {
+            return target.ban({ reason: `User banned by ${message.author.tag} for reason: **Reason not provided**` })
+                .catch(error => {
+                    message.channel.send(new Discord.MessageEmbed()
+                        .setDescription(`<:cross:729019052571492434> Ban failed: \`${error}\``)
+                        .setColor(`#FF3838`));
+                });
+        }
     }
 };

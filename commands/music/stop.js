19ef91d88dbc6db8584d29ddd4f42252bde1d830
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2020-09-07 01:21:26 -0700

    Updated to use new music data system

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -13,7 +13,7 @@ module.exports = {
     type: 'music',
     execute(message, args) {
 
-        var dispatcher = index.getDispatcher(message);
+        let dispatcher = message.guild.music.dispatcher;
 
         if (dispatcher == undefined || dispatcher.speaking == false) {
             return message.channel.send(new Discord.MessageEmbed()
@@ -21,9 +21,15 @@ module.exports = {
                 .setColor(`#FF3838`));
         }
 
-        index.setQueue(message, index.constructQueue());
-        index.endDispatcher(message);
-        index.setDispatcher(message, undefined);
+        // Empty queue
+        message.guild.music.queue = [];
+
+        // End dispatcher
+        message.guild.music.dispatcher.destroy();
+
+        // Empty dispatcher
+        message.guild.music.dispatcher = [];
+
         client.voice.connections.get(message.guild.id).disconnect();
 
         message.channel.send(new Discord.MessageEmbed()

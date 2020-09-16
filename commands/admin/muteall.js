2bb238767c183f9fcf9b1e4eef1301a41defe78e
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2020-09-15 17:38:53 -0700

    Add check for if no voice channel

--- commands/admin/muteall.js
+++ commands/admin/muteall.js
@@ -17,9 +17,15 @@ module.exports = {
     type: 'admin',
     execute(message, args) {
         const channel = message.member.voice.channel;
+
+        if (!channel) return message.channel.send(new Discord.MessageEmbed()
+            .setDescription(`<:cross:729019052571492434> ${message.author.username}, you are not in a voice channel`)
+            .setColor(`#FF3838`));
+
         for (const member of channel.members) {
             member[1].voice.setMute(true);
         }
+
         return message.channel.send(new Discord.MessageEmbed()
             .setDescription(`:mute: Muted all participants`)
             .setColor(`#36393f`));

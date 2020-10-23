a4db91920f43741da2e7aeb9ff9463ab540537c7
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2020-10-22 18:21:59 -0700

    Show global leaderboard alongside server

--- commands/currency/leaderboard.js
+++ commands/currency/leaderboard.js
@@ -15,12 +15,21 @@ module.exports = {
     execute(message, args) {
         const client = message.client;
         return message.channel.send(new Discord.MessageEmbed()
-            .setDescription(`:medal: **Top 10 users by currency**\n\n` + currency.sort((a, b) => b.balance - a.balance)
+            .setTitle(`:medal: Top 10 richest users`)
+            .setDescription(`\u200b`)
+            .addField(`:desktop: ${message.guild.name}'s Leaderboard`, currency.sort((a, b) => b.balance - a.balance)
                 .filter(user => client.users.cache.has(user.user_id) && message.guild.member(client.users.cache.get(user.user_id)))
                 .first(10)
                 .map((user, position) => `\`${position + 1}.\` **${(client.users.cache.get(user.user_id).username)}**\nBalance: \`$${user.balance}\`\n`)
                 .join('\n'),
-                { code: true })
+                true)
+            .addField('\u200b', '\u200b', true)
+            .addField(`:earth_americas: Global Leaderboard`, currency.sort((a, b) => b.balance - a.balance)
+                .filter(user => client.users.cache.has(user.user_id))
+                .first(10)
+                .map((user, position) => `\`${position + 1}.\` **${(client.users.cache.get(user.user_id).username)}**\nBalance: \`$${user.balance}\`\n`)
+                .join('\n'),
+                true)
             .setColor(`#1b9e56`));
     }
 };

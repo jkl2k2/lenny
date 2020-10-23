c1daad1400c8968e067df79b17c86c2a13b52548
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2020-10-23 15:08:05 -0700

    Also log eval result to console in case it's too big for text

--- commands/admin/eval.js
+++ commands/admin/eval.js
@@ -31,6 +31,8 @@ module.exports = {
 			if (typeof evaled !== "string")
 				evaled = require("util").inspect(evaled);
 
+			console.log(clean(evaled));
+
 			message.channel.send(clean(evaled), { code: "xl" });
 		} catch (err) {
 			message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);

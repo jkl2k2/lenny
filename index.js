0b60729fca72dd0e1e294a7072e281a788c288b0
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2020-10-26 14:59:50 -0700

    Create userCasinoPity Enmap

--- index.js
+++ index.js
@@ -128,6 +128,17 @@ client.stats.default = {
     musicTime: 0
 };
 
+client.userCasinoPity = new Enmap({
+    name: "userCasinoPity",
+    fetchAll: false,
+    autoFetch: true,
+    cloneLevel: 'deep'
+});
+
+client.userCasinoPity.default = {
+    losingStreak: 0
+};
+
 client.on(`guildDelete`, guild => {
     // Remove deleted guild from Enmap
     client.settings.delete(guild.id);

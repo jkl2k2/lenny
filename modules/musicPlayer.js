4c9155bb241187bacd2e58e01009e89d3d3e2c45
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2020-11-21 12:03:51 -0800

    Listen to "close" event for dispatcher

--- modules/musicPlayer.js
+++ modules/musicPlayer.js
@@ -55,6 +55,8 @@ const sendDetails = async (input, c) => {
 
 //#region play
 const play = async message => {
+    logger.debug(`Beginning play() command execution`);
+
     const client = message.client;
 
     const queue = message.guild.music.queue;
@@ -65,11 +67,19 @@ const play = async message => {
     if (queue[0].getType() == "video" || queue[0].getType() == "livestream") {
         // If regular video
 
+        logger.debug(`Input is a regular video`);
+
         // Download YouTube video
-        const input = ytdl(queue[0].getURL(), { quality: "highestaudio" });
+        const input = ytdl(queue[0].getURL());
+
+        if (input) {
+            logger.debug(`YTDL output is NOT undefined`);
+        } else {
+            logger.debug(`YTDL output IS undefined`);
+        }
 
         // Set dispatcher
-        message.guild.music.dispatcher = client.voice.connections.get(message.guild.id).play(input, { bitrate: 384, volume: message.guild.music.volume, passes: 5, fec: true });
+        message.guild.music.dispatcher = client.voice.connections.get(message.guild.id).play(input, { bitrate: 128, volume: message.guild.music.volume, passes: 5, fec: true });
 
         // Mark server as playing music
         message.guild.music.playing = true;
@@ -92,6 +102,8 @@ const play = async message => {
     } else if (queue[0].getType() == "soundcloud") {
         // If SoundCloud
 
+        logger.debug(`Input is a SoundCloud song`);
+
         // Download SoundCloud song
         scdl.download(queue[0].getURL())
             .then(stream => {
@@ -106,6 +118,8 @@ const play = async message => {
             });
     } else if (queue[0].getType() == "radio") {
         // If radio station
+
+        logger.debug(`Input is a radio station`);
 
         // Get stream URL
         const input = await iheart.streamURL(queue[0].getStation());
@@ -131,20 +145,9 @@ const play = async message => {
     // Reset dispatcher stream delay
     client.voice.connections.get(message.guild.id).player.streamingData.pausedTime = 0;
 
-    /*
-    message.guild.music.dispatcher.on("close", () => {
-        if (message.guild.music.repeat) {
-            queue.unshift(message.guild.music.lastPlayed);
-        }
-        if (queue[0]) {
-            return play(message);
-        } else {
-            message.guild.music.playing = false;
-        }
-    });
-    */
+    message.guild.music.dispatcher.on("close", async () => {
+        logger.debug(`Dispatcher fired "close" event`);
 
-    message.guild.music.dispatcher.on("finish", () => {
         // Add time playing to server stats
         const serverStats = client.stats.ensure(message.guild.id, client.stats.default);
 
@@ -155,12 +158,45 @@ const play = async message => {
         if (message.guild.music.repeat) {
             queue.unshift(message.guild.music.lastPlayed);
         }
+
         if (queue[0]) {
-            return play(message);
+            logger.debug(`queue[0] is ${queue[0].getTitle()}, calling play()`);
+            message.guild.music.dispatcher = undefined;
+            return await play(message);
         } else {
+            console.log(`queue[0] NOT found, stopping and resetting playing`);
             message.guild.music.playing = false;
+            message.guild.music.dispatcher = undefined;
         }
     });
+
+    message.guild.music.dispatcher.on("finish", async () => {
+        logger.debug(`Dispatcher fired "finish" event`);
+
+        /*
+        // Add time playing to server stats
+        const serverStats = client.stats.ensure(message.guild.id, client.stats.default);
+
+        if (message.guild.music.dispatcher != undefined && message.guild.music.dispatcher.streamTime != undefined) {
+            client.stats.set(message.guild.id, serverStats[`musicTime`] + message.guild.music.dispatcher.streamTime, `musicTime`);
+        }
+
+        if (message.guild.music.repeat) {
+            queue.unshift(message.guild.music.lastPlayed);
+        }
+
+        if (queue[0]) {
+            logger.debug(`queue[0] is ${queue[0].getTitle()}, calling play()`);
+            message.guild.music.dispatcher = undefined;
+            return await play(message);
+        } else {
+            console.log(`queue[0] NOT found, stopping and resetting playing`);
+            message.guild.music.playing = false;
+            message.guild.music.dispatcher = undefined;
+        }
+        */
+    });
+
 };
 
 //#endregion

deceea2cc6880355170ecb9c78a62a69480d16fb
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2020-12-08 21:36:10 -0800

    Removes the "--no-deprecation" flag from the node call

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 
-npm install --production && npm update /m && npm install ytdl-core@latest && node index.js --no-deprecation
+npm install --production && npm update /m && npm install ytdl-core@latest && node index.js
 
 pause

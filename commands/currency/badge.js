060839caccbb1bc0eb8da037491979aaf54d434b
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2020-12-22 15:02:00 -0800

    Badges are now ranks with names

--- commands/currency/badge.js
+++ commands/currency/badge.js
@@ -1,14 +1,44 @@
 const index = require(`../../index.js`);
 const { MessageEmbed } = require(`discord.js`);
+const { sub } = require("ffmpeg-static");
 const currency = index.getCurrencyDB();
 
 function readableNum(number) {
     return number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
 
+function levelToRank(level) {
+    if (level == 0) {
+        return `Unranked`;
+    }
+
+    const ranks = [`Bronze`, `Silver`, `Gold`, `Platinum`, `Diamond`, `How do you have this`];
+    const subRanks = [`I`, `II`, `III`];
+
+    let determinedSub = 0;
+
+    for (let i = 0; i < level; i++) {
+        if (i % 3 == 0) {
+            determinedSub = 0;
+        } else {
+            determinedSub++;
+        }
+    }
+
+    let determinedRank = 0;
+
+    for (let i = 0; i < level; i++) {
+        if (i % 3 == 0 && i != 0) {
+            determinedRank++;
+        }
+    }
+
+    return `${ranks[determinedRank]} ${subRanks[determinedSub]}`;
+}
+
 module.exports = {
     name: 'badge',
-    description: 'Use your money to buy badges',
+    description: 'Buy badges and get perks',
     aliases: ['badges'],
     // args: true,
     // usage: '[command]',
@@ -29,18 +59,17 @@ module.exports = {
             // If no args provided
             return message.channel.send(new MessageEmbed()
                 .setAuthor(message.author.username, message.author.avatarURL())
-                .addField(`Badge level`, `\`Badge ${casinoUser[`badgeLevel`]}\``, true)
+                .addField(`Badge rank`, `\`${levelToRank(casinoUser[`badgeLevel`])}\``, true)
                 // .addField(`Prestige level`, `\`Prestige ${casinoUser[`prestigeLevel`]}\``, true)
-                .addField(`Next badge`, `\`Badge ${casinoUser[`badgeLevel`] + 1}\` (**$${readableNum(Math.pow(10, 4 + casinoUser[`badgeLevel`]))}**)`)
+                .addField(`Next rank`, `\`${levelToRank(casinoUser[`badgeLevel`] + 1)}\` (**$${readableNum(Math.pow(10, 4 + casinoUser[`badgeLevel`]))}**)`)
                 .setColor(`#36393f`));
-
         } else if (args[0] == `upgrade`) {
             // If user wants to upgrade
             // Check if user can afford upgrade
             if (currency.getBalance(message.author.id) >= Math.pow(10, 4 + casinoUser[`badgeLevel`])) {
                 // Show upgrade info
                 message.channel.send(new MessageEmbed()
-                    .setDescription(`<:check:728881238970073090> You can afford to upgrade your badge to \`Badge ${casinoUser[`badgeLevel`] + 1}\` for \`$${readableNum(Math.pow(10, 4 + casinoUser[`badgeLevel`]))}\`\n\nCurrent balance: \`$${readableNum(currency.getBalance(message.author.id))}\`\nBalance after: \`$${readableNum(currency.getBalance(message.author.id) - Math.pow(10, 4 + casinoUser[`badgeLevel`]))}\`\n\nWould you like to upgrade? **(Y/N)**`)
+                    .setDescription(`<:check:728881238970073090> You can afford to upgrade your badge to \`${levelToRank(casinoUser[`badgeLevel`] + 1)}\` for \`$${readableNum(Math.pow(10, 4 + casinoUser[`badgeLevel`]))}\`\n\nCurrent balance: \`$${readableNum(currency.getBalance(message.author.id))}\`\nBalance after: \`$${readableNum(currency.getBalance(message.author.id) - Math.pow(10, 4 + casinoUser[`badgeLevel`]))}\`\n\nWould you like to upgrade? **(Y/N)**`)
                     .setColor(`#2EC14E`));
 
                 // Listen for response
@@ -63,7 +92,7 @@ module.exports = {
 
                         // Show confirmation of upgrade
                         return message.channel.send(new MessageEmbed()
-                            .setDescription(`:tada: You have now reached \`Badge ${casinoUser[`badgeLevel`] + 1}\`!\n\nPrevious balance: \`$${readableNum(oldBalance)}\`\nNew balance: \`$${readableNum(currency.getBalance(message.author.id))}\`\n\nNext up is \`Badge ${casinoUser[`badgeLevel`] + 2}\` (\`$${readableNum(Math.pow(10, 4 + casinoUser[`badgeLevel`] + 1))}\`)`)
+                            .setDescription(`:tada: You have now reached \`${levelToRank(casinoUser[`badgeLevel`] + 1)}\`!\n\nPrevious balance: \`$${readableNum(oldBalance)}\`\nNew balance: \`$${readableNum(currency.getBalance(message.author.id))}\`\n\nNext up is \`${levelToRank(casinoUser[`badgeLevel`] + 2)}\` (\`$${readableNum(Math.pow(10, 4 + casinoUser[`badgeLevel`] + 1))}\`)`)
                             .setColor(`#2EC14E`));
 
                     } else if (userResponse == `n` || userResponse == `no`) {
@@ -81,7 +110,7 @@ module.exports = {
             } else {
                 // User cannot afford badge upgrade
                 message.channel.send(new MessageEmbed()
-                    .setDescription(`<:cross:729019052571492434> You cannot afford to upgrade your badge to \`Badge ${casinoUser[`badgeLevel`] + 1}\`\n\nCurrent balance: \`$${readableNum(currency.getBalance(message.author.id))}\`\nBadge costs: \`$${readableNum(Math.pow(10, 4 + casinoUser[`badgeLevel`]))}\``)
+                    .setDescription(`<:cross:729019052571492434> You cannot afford to upgrade your badge to \`${levelToRank(casinoUser[`badgeLevel`] + 1)}\`\n\nCurrent balance: \`$${readableNum(currency.getBalance(message.author.id))}\`\nBadge costs: \`$${readableNum(Math.pow(10, 4 + casinoUser[`badgeLevel`]))}\``)
                     .setColor(`#FF3838`));
             }
         }

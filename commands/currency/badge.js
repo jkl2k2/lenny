9da6775ef2d2789645af06d1d10b2c8c88d01147
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2020-12-23 10:22:54 -0800

    Added !badge max to instantly upgrade to the highest possible badge

--- commands/currency/badge.js
+++ commands/currency/badge.js
@@ -113,6 +113,65 @@ module.exports = {
                     .setDescription(`<:cross:729019052571492434> You cannot afford to upgrade your badge to \`${levelToRank(casinoUser[`badgeLevel`] + 1)}\`\n\nCurrent balance: \`$${readableNum(currency.getBalance(message.author.id))}\`\nBadge costs: \`$${readableNum(Math.pow(10, 4 + casinoUser[`badgeLevel`]))}\``)
                     .setColor(`#FF3838`));
             }
+        } else if (args[0] == `max`) {
+            // If user wants to upgrade
+            // Check if user can afford upgrade
+            let totalCost = 0;
+            let upgradeCount = 4;
+
+            for (let i = Math.pow(10, 4 + casinoUser[`badgeLevel`]); i < currency.getBalance(message.author.id); i += Math.pow(10, upgradeCount)) {
+                console.log(i);
+                totalCost = i;
+                upgradeCount++;
+            }
+
+            if (currency.getBalance(message.author.id) >= totalCost) {
+                // Show upgrade info
+                message.channel.send(new MessageEmbed()
+                    .setDescription(`<:check:728881238970073090> You can afford to upgrade your badge **${upgradeCount - 4} times** to \`${levelToRank(casinoUser[`badgeLevel`] + (upgradeCount - 4))}\` for \`$${readableNum(totalCost)}\`\n\nCurrent balance: \`$${readableNum(currency.getBalance(message.author.id))}\`\nBalance after: \`$${readableNum(currency.getBalance(message.author.id) - totalCost)}\`\n\nWould you like to upgrade? **(Y/N)**`)
+                    .setColor(`#2EC14E`));
+
+                // Listen for response
+                const filter = response => response.author.id == message.author.id && (response.content.toLowerCase() == `y` || response.content.toLowerCase() == `n` || response.content.toLowerCase() == `yes` || response.content.toLowerCase() == `no`);
+
+                const collector = message.channel.createMessageCollector(filter, { time: 60000, max: 1 });
+
+                collector.on(`collect`, async response => {
+                    const userResponse = response.content.toLowerCase();
+
+                    if (userResponse == `y` || userResponse == `yes`) {
+                        // Store old balance
+                        const oldBalance = currency.getBalance(message.author.id);
+
+                        // Collect payment
+                        currency.add(message.author.id, -totalCost);
+
+                        // Increase badge level
+                        client.casinoUser.set(message.author.id, casinoUser[`badgeLevel`] + (upgradeCount - 4), `badgeLevel`);
+
+                        // Show confirmation of upgrade
+                        return message.channel.send(new MessageEmbed()
+                            .setDescription(`:tada: You have now reached \`${levelToRank(casinoUser[`badgeLevel`] + (upgradeCount - 4))}\`!\n\nPrevious balance: \`$${readableNum(oldBalance)}\`\nNew balance: \`$${readableNum(currency.getBalance(message.author.id))}\`\n\nNext up is \`${levelToRank(casinoUser[`badgeLevel`] + 1 + (upgradeCount - 4))}\` (\`$${readableNum(Math.pow(10, casinoUser[`badgeLevel`] + upgradeCount))}\`)`)
+                            .setColor(`#2EC14E`));
+
+                    } else if (userResponse == `n` || userResponse == `no`) {
+                        return message.channel.send(new MessageEmbed()
+                            .setDescription(`:information_source: Canceled badge upgrade`)
+                            .setColor(`#0083FF`));
+                    }
+                });
+                collector.on('end', collected => {
+                    // Name entry timed out
+                    if (!collected.size) return message.channel.send(new MessageEmbed()
+                        .setDescription(`<:cross:729019052571492434> Badge upgrade response timed out`)
+                        .setColor(`#FF3838`));
+                });
+            } else {
+                // User cannot afford badge upgrade
+                message.channel.send(new MessageEmbed()
+                    .setDescription(`<:cross:729019052571492434> You cannot afford to upgrade your badge\n\nCurrent balance: \`$${readableNum(currency.getBalance(message.author.id))}\`\nNext badge costs: \`$${readableNum(Math.pow(10, 4 + casinoUser[`badgeLevel`]))}\``)
+                    .setColor(`#FF3838`));
+            }
         }
     }
 };

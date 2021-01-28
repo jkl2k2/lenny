2eb43c87ca7717745d4cd1d0d4970635deb3b973
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2021-01-27 22:08:31 -0800

    Added a refresh button to the message to allow for a reprocess

--- commands/fun/gpt.js
+++ commands/fun/gpt.js
@@ -3,9 +3,48 @@ const config = require(`config`);
 const GPTKEY = config.get(`Bot.GPTKEY`);
 const { MessageEmbed } = require(`discord.js`);
 
+function reactionHandler(sent, message, args) {
+    const client = message.client;
+
+    const filter = (reaction, user) => {
+        return ['🔄'].includes(reaction.emoji.name) && user.id === message.author.id;
+    };
+
+    sent.react(`🔄`)
+        .catch(() => console.error(`One of the emojis failed to react.`));
+
+    sent.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+        .then(async collected => {
+            const reaction = collected.first();
+
+            if (reaction.emoji.name === `🔄`) {
+                sent.delete();
+
+                const newSent = await message.channel.send(new MessageEmbed()
+                    .setDescription(`:arrows_counterclockwise: Processing response...`)
+                    .setFooter(`Processing time depends on your input length`)
+                    .setColor(`#54c3ff`));
+
+                const outList = await booste.gpt2(GPTKEY, args.join(` `), 30, 0.8, 30 + args.length);
+
+                console.log(outList.join(` `));
+
+                newSent.edit(new MessageEmbed()
+                    .setAuthor(`Response to ${message.author.username}`, message.author.avatarURL())
+                    .setDescription(`**${args.join(` `)}** ${outList.join(` `)}`)
+                    .setColor(`#54c3ff`));
+
+                return reactionHandler(newSent, message, args);
+            }
+        })
+        .catch(async collected => {
+            sent.reactions.removeAll();
+        });
+}
+
 module.exports = {
     name: 'gpt',
-    description: 'GPT2 text completion. Processing takes about 30 seconds. Not every response will be good.',
+    description: 'GPT2 text completion. Processing takes about 15-20 seconds. Not every response will be good.',
     aliases: ['gpt2'],
     args: true,
     usage: '[text prompt]',
@@ -23,14 +62,16 @@ module.exports = {
 
         const sent = await message.channel.send(new MessageEmbed()
             .setDescription(`:arrows_counterclockwise: Processing response...`)
-            .setFooter(`Processing takes about 30 seconds on average`)
+            .setFooter(`Processing time depends on your input length`)
             .setColor(`#54c3ff`));
 
-        const outList = await booste.gpt2(GPTKEY, args.join(` `), 30);
+        const outList = await booste.gpt2(GPTKEY, args.join(` `), 30, 0.8, 30 + args.length);
 
         sent.edit(new MessageEmbed()
-            .setAuthor(`Lenny says:`, client.user.avatarURL())
+            .setAuthor(`Response to ${message.author.username}`, message.author.avatarURL())
             .setDescription(`**${args.join(` `)}** ${outList.join(` `)}`)
             .setColor(`#54c3ff`));
+
+        reactionHandler(sent, message, args);
     }
 };

b7e9deebecb8f4f11ae929c124f5d5dff30171ea
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2021-05-01 00:12:38 -0700

    Added load and unload commands

---
+++ src/commands/load.js
@@ -0,0 +1,38 @@
+const { Command } = require(`discord-akairo`);
+const { MessageEmbed } = require(`discord.js`);
+
+class LoadCommand extends Command {
+    constructor() {
+        super(`load`, {
+            aliases: [`load`],
+            args: [
+                {
+                    id: `commandID`
+                }
+            ],
+            ownerOnly: true,
+            category: `owner`
+        });
+    }
+
+    exec(message, args) {
+        // Really should not be doing this but I can't figure any other way out
+        const commandPath = `C:\\Users\\Joshua\\Desktop\\Bots\\Lenny Akairo\\src\\commands\\${args.commandID}.js`;
+
+        // "this" refers to the command object
+        try {
+            this.handler.load(commandPath);
+        } catch (error) {
+            console.debug(error);
+            return message.channel.send(new MessageEmbed()
+                .setDescription(`<:cross:729019052571492434> Error loading command module \`${args.commandID}\`\n\`\`\`${error}\`\`\``)
+                .setColor(`#36393f`));
+        }
+
+        return message.channel.send(new MessageEmbed()
+            .setDescription(`:arrow_right: Loaded command module \`${args.commandID}\``)
+            .setColor(`#36393f`));
+    }
+}
+
+module.exports = LoadCommand;
---
+++ src/commands/unload.js
@@ -0,0 +1,34 @@
+const { Command } = require(`discord-akairo`);
+const { MessageEmbed } = require(`discord.js`);
+
+class UnloadCommand extends Command {
+    constructor() {
+        super(`unload`, {
+            aliases: [`unload`],
+            args: [
+                {
+                    id: `commandID`
+                }
+            ],
+            ownerOnly: true,
+            category: `owner`
+        });
+    }
+
+    exec(message, args) {
+        // "this" refers to the command object
+        try {
+            this.handler.remove(args.commandID);
+        } catch (error) {
+            console.debug(error);
+            return message.channel.send(new MessageEmbed()
+                .setDescription(`<:cross:729019052571492434> Error unloading command module \`${args.commandID}\`\n\`\`\`${error}\`\`\``)
+                .setColor(`#36393f`));
+        }
+        return message.channel.send(new MessageEmbed()
+            .setDescription(`:eject: Unloaded command module \`${args.commandID}\``)
+            .setColor(`#36393f`));
+    }
+}
+
+module.exports = UnloadCommand;

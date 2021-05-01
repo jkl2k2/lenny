40d505081561fd2e1c4f112ba90585495b219da1
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2021-05-01 00:49:53 -0700

    Extend Guild to support music features

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 // Requires
 const result = require(`dotenv`).config();
 const Akairo = require(`./akairo`);
+const { Structures } = require(`discord.js`);
 
 // Throw if dotenv error
 if (result.error) throw result.error;
+
+// Extend Guild to support music
+Structures.extend('Guild', Guild => {
+    class ExtendedGuild extends Guild {
+        constructor(client, data) {
+            super(client, data);
+            this.music = {
+                queue: [],
+                lastPlayed: undefined,
+                lastEmbed: undefined,
+                playing: false,
+                paused: false,
+                repeat: false,
+                volume: 1,
+                oldVolume: 1,
+                dispatcher: undefined,
+            };
+        }
+    }
+    return ExtendedGuild;
+});
 
 // Create client
 const client = new Akairo();

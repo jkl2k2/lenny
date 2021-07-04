b006c50b8a7f79d08f0116ed9428421aaffc86a3
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2021-07-04 16:04:06 -0700

    Create MusicSubscription class

---
+++ src/modules/subscription.js
@@ -0,0 +1,131 @@
+const {
+    AudioPlayerStatus,
+    createAudioPlayer,
+    entersState,
+    VoiceConnectionDisconnectReason,
+    VoiceConnectionStatus,
+} = require(`@discordjs/voice`);
+
+function wait(time) {
+    return new Promise(resolve => setTimeout(resolve, time).unref());
+}
+
+/**
+ * A MusicSubscription exists for each active VoiceConnection. Each subscription has its own audio player and queue,
+ * and it also attaches logic to the audio player and voice connection for error handling and reconnection logic.
+ */
+module.exports = class MusicSubscription {
+    /**
+     * The VoiceConnection this subscription is attached to.
+     */
+    constructor(voiceConnection) {
+        this.voiceConnection = voiceConnection;
+        this.audioPlayer = createAudioPlayer();
+        this.queue = [];
+        this.queueLock = false;
+        this.readyLock = false;
+
+        this.voiceConnection.on(`stateChange`, async (_, newState) => {
+            if (newState.status === VoiceConnectionStatus.Disconnected) {
+                if (newState.reason === VoiceConnectionDisconnectReason.WebSocketClose && newState.closeCode == 4014) {
+                    // Code 4014 means we should not make a manual reconnect attempt
+                    try {
+                        await entersState(this.voiceConnection, VoiceConnectionStatus.Connecting, 5000);
+                        // Probably moved voice channel
+                    } catch (err) {
+                        this.voiceConnection.destroy();
+                        // Probably removed from voice channel
+                    }
+                } else if (this.voiceConnection.rejoinAttempts < 5) {
+                    // Recoverable disconnect
+                    await wait((this.voiceConnection.rejoinAttempts + 1) * 5000);
+                    this.voiceConnection.rejoin();
+                } else {
+                    // Unrecoverable disconnect, no more remaining attempts to reconnect, no choice but to destroy
+                    this.voiceConnection.destroy();
+                }
+            } else if (newState.status === VoiceConnectionStatus.Destroyed) {
+                // When destroyed, stop the subscription
+                this.stop();
+            } else if (!this.readyLock && (newState.status === VoiceConnectionStatus.Connecting || newState.status === VoiceConnectionStatus.Signalling)) {
+                /*
+                    In the Signalling or Connecting states, we set a 20 second time limit for the connection to become ready
+                    before destroying the voice connection. This stops the voice connection permanently existing in one of these
+                    states.
+                */
+                this.readyLock = true;
+                try {
+                    await entersState(this.voiceConnection, VoiceConnectionStatus.Ready, 20000);
+                } catch (err) {
+                    if (this.voiceConnectionl.state.status !== VoiceConnectionStatus.Destroyed) this.voiceConnection.destroy();
+                } finally {
+                    this.readyLock = false;
+                }
+            }
+        });
+
+        // Configure audio player
+        this.audioPlayer.on(`stateChange`, (oldState, newState) => {
+            if (newState.status === AudioPlayerStatus.Idle && oldState.status !== AudioPlayerStatus.Idle) {
+                // Entered idle from non-idle state, need to play next track
+                oldState.resource.metadata.onFinish();
+                this.processQueue();
+            } else if (newState.status === AudioPlayerStatus.Playing) {
+                // Entered playing state, new track has started
+                newState.resource.metadata.onStart();
+            }
+        });
+
+        this.audioPlayer.on(`error`, (err) => {
+            error.resource.metadata.onError(err);
+        });
+
+        voiceConnection.subscribe(this.audioPlayer);
+    }
+
+    /**
+     * Adds a new Track to the queue.
+     *
+     * @param track The track to add to the queue
+     */
+    enqueue(track) {
+        this.queue.push(track);
+        this.processQueue();
+    }
+
+    /**
+     * Stop audio playback, empties queue
+     */
+    stop() {
+        this.queueLock = true;
+        this.queue = [];
+        this.audioPlayer.stop(true);
+    }
+
+    /**
+     * Attempts to play a track from the queue
+     */
+    async processQueue() {
+        // Return if queue is locked, empty, or audio player is already active
+        if (this.queueLock || this.audioPlayer.state.status !== AudioPlayerStatus.Idle || this.queue.length === 0) return;
+
+        // Lock queue for safety
+        this.queueLock = true;
+
+        // Get next track
+        const nextTrack = this.queue.shift();
+        try {
+            console.log(`Playing ${nextTrack.title}`);
+            // Convert Track to AudioResource
+            const resource = await nextTrack.createAudioResource();
+            this.audioPlayer.play(resource);
+            this.queueLock = false;
+        } catch (err) {
+            console.log(err);
+            // On fail, try next item of queue instead
+            nextTrack.onError(err);
+            this.queueLock = false;
+            return this.processQueue();
+        }
+    }
+};

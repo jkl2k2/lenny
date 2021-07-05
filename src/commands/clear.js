e20b77950b5c29a35bce418b23cd2cb540db3add
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2021-07-05 01:09:47 -0700

    Removed functionality of clear command Avoids crashing bot

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -12,16 +12,7 @@ class ClearCommand extends Command {
     }
 
     exec(message) {
-        if (message.guild.music.queue == undefined || message.guild.music.queue.length == 0) {
-            return message.channel.send(new MessageEmbed()
-                .setDescription(`<:cross:729019052571492434> There is nothing to skip`)
-                .setColor(`#FF3838`));
-        } else {
-            message.guild.music.queue = [];
-            return message.channel.send(new MessageEmbed()
-                .setDescription(`:arrow_double_up: ${message.author.username} cleared the queue`)
-                .setColor(`#36393f`));
-        }
+        return;
     }
 }
 

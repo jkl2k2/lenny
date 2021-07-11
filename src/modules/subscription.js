f5464eb71c855bc453de9a0422fce4a24fb3d30f
jkl2k2 <24350667+jkl2k2@users.noreply.github.com>
2021-07-10 22:06:40 -0700

    Add shuffle functionality to MusicSubscription

--- src/modules/subscription.js
+++ src/modules/subscription.js
@@ -10,6 +10,22 @@ function wait(time) {
     return new Promise(resolve => setTimeout(resolve, time).unref());
 }
 
+function arrayShuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
 /**
  * A MusicSubscription exists for each active VoiceConnection. Each subscription has its own audio player and queue,
  * and it also attaches logic to the audio player and voice connection for error handling and reconnection logic.
@@ -111,6 +127,13 @@ module.exports = class MusicSubscription {
      */
     clearQueue() {
         this.queue = [];
+    }
+
+    /**
+     * Shuffle the queue
+     */
+    shuffle() {
+        this.queue = arrayShuffle(this.queue);
     }
 
     /**

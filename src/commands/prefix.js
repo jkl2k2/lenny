5cc282b7f3b1afa0e065a4b3c87736c6b6d835c6
Joshua Lomelin <24350667+jkl2k2@users.noreply.github.com>
2021-10-26 10:02:29 -0700

    Remove Akairo argument validation Replaced with manual validation written by me

--- src/commands/prefix.js
+++ src/commands/prefix.js
@@ -8,44 +8,7 @@ class PrefixCommand extends Command {
             aliases: [`prefix`],
             args: [
                 {
-                    id: `newPrefix`,
-                    type: Argument.validate('string', (m, p, str) => str.length <= 3),
-                    prompt: {
-                        start: () => {
-                            const embed = new MessageEmbed()
-                                .setDescription(`:arrow_right: What would you like to set the prefix to? **Limit of 3 characters.**`)
-                                .setFooter(`Type "cancel" to stop`)
-                                .setColor(`#36393f`);
-                            return embed;
-                        },
-                        retry: () => {
-                            const embed = new MessageEmbed()
-                                .setDescription(`<:cross:729019052571492434> That prefix is not valid. Please send another one instead.`)
-                                .setFooter(`Type "cancel" to stop`)
-                                .setColor(`#FF3838`);
-                            return embed;
-                        },
-                        cancel: () => {
-                            const embed = new MessageEmbed()
-                                .setDescription(`<:cross:729019052571492434> Prefix change canceled.`)
-                                .setColor(`#FF3838`);
-                            return embed;
-                        },
-                        ended: () => {
-                            const embed = new MessageEmbed()
-                                .setDescription(`<:cross:729019052571492434> Too many retries, prefix change canceled.`)
-                                .setColor(`#FF3838`);
-                            return embed;
-                        },
-                        timeout: () => {
-                            const embed = new MessageEmbed()
-                                .setDescription(`<:cross:729019052571492434> Prefix change timed out.`)
-                                .setColor(`#FF3838`);
-                            return embed;
-                        },
-                        retries: 3,
-                        time: 30000
-                    }
+                    id: `newPrefix`
                 }
             ],
             category: `admin`,
@@ -57,6 +20,16 @@ class PrefixCommand extends Command {
 
     exec(message, args) {
         const client = message.client;
+
+        if (!args.newPrefix || args.newPrefix.length > 5 || args.newPrefix.length < 1) {
+            return message.channel.send({
+                embeds: [
+                    new MessageEmbed()
+                        .setDescription(`<:cross:729019052571492434> Prefix must be less than 5 characters and at least 1 character long`)
+                        .setColor(`#FF3838`)
+                ]
+            });
+        }
 
         client.settings.ensure(message.guild.id, client.settings.default);
 

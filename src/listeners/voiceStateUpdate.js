1d6bb1bf82ff4c08738847d501b20e0bd446f987
Joshua Lomelin <24350667+jkl2k2@users.noreply.github.com>
2021-10-26 09:32:56 -0700

    Comment out logging

--- src/listeners/voiceStateUpdate.js
+++ src/listeners/voiceStateUpdate.js
@@ -9,7 +9,7 @@ class VoiceStateUpdateListener extends Listener {
     }
 
     exec(oldState, newState) {
-        console.log(`voiceStateUpdate emitted`);
+        // console.log(`voiceStateUpdate emitted`);
         if ((oldState.member.id !== this.client.user.id && oldState.channel && oldState.channel.members.size === 1 && oldState.channel.members.has(this.client.user.id)) || (oldState.channel && !newState.channel)) {
             const subscription = this.client.subscriptions.get(oldState.guild.id);
 

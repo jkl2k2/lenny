536ded82d141e2175464b416ac36f63ae1103e21
Joshua Lomelin <24350667+jkl2k2@users.noreply.github.com>
2022-01-18 12:17:47 -0800

    Test log of server's musicTime stats

--- src/commands/test.js
+++ src/commands/test.js
@@ -13,17 +13,9 @@ class TestCommand extends Command {
     }
 
     exec(message) {
-        const guild = message.guild;
+        const serverStats = message.client.stats.ensure(message.guild.id, message.client.stats.default);
 
-        const channels = guild.channels.cache.filter(channel => channel.type == `GUILD_TEXT`);
-
-        channels.first().send({
-            embeds: [
-                new MessageEmbed()
-                    .setAuthor(`Lenny Bot v5 Beta`, message.client.user.avatarURL())
-                    .setDescription(`Hey there! Keep in mind that this bot is under active development. It will sometimes not work as expected and will randomly crash or restart for updates.\n\nAlso, for the sake of transparency, know that commands and error messages are logged for debugging purposes. Nothing else is logged.\n\nDefault prefix is \`!\`, but can be changed.\n\nMy spaghetti code is open-source on [GitHub](https://github.com/jkl2k2/lenny).\n\nContact jkl2k2#6035 if there are any issues.`)
-            ]
-        }).catch(e => console.log(e));
+        console.log(`music for ${Math.floor(serverStats[`musicTime`] / 1000 / 60 / 60)} hours`);
     }
 }
 

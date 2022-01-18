845f7079e94fd0233c70d329e5695a21e2b694e8
Joshua Lomelin <24350667+jkl2k2@users.noreply.github.com>
2022-01-18 12:18:06 -0800

    Update comments

--- src/modules/track.js
+++ src/modules/track.js
@@ -19,7 +19,7 @@ module.exports = class Track {
     /**
      * Creates a new Track.
      * @param {Video} video The full video/song object returned by play-dl
-     * @param {User} requester The user that requested this track
+     * @param {GuildMember} requester The user that requested this track
      * @param {function} onStart A function to call when the track is started.
      * @param {function} onFinish A function to call when the track is finished.
      * @param {function} onError A function to call when the track has an error.
@@ -104,7 +104,7 @@ module.exports = class Track {
      * Creates a Track from a video URL and lifecycle callback methods.
      *
      * @param url The URL of the video
-     * @param requester The User that requested the song
+     * @param requester The GuildMember that requested the song
      * @param methods Lifecycle callbacks
      * @return The created Track
      */
